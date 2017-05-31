397920aa5f6b077efa60c97115e46acffa3cd4dc
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-05-31 12:36:54 +0100

    MuPad part of HodgkinHuxley example runs (but produces slightly different output, was perhaps run with different settings?

--- thirdparty/SENSAI_MuPad/examples/ODE_examples/HodgkinHuxley/user_inputs.m
+++ thirdparty/SENSAI_MuPad/examples/ODE_examples/HodgkinHuxley/user_inputs.m
@@ -1,52 +1,24 @@
-function [DIR,JOB,imap,x0,p0,tfinal,solntimes,ntsteps,qtype,stype,NextGen,R0_only]=user_inputs 
-%  ***   [DIR,JOB,imap,x0,p0,tfinal,solntimes,ntsteps,qtype,stype,NextGen,R0_only]=user_inputs  ***
-%
-% Inputs
-%  DIR -> Directory in which results are to be stored
-%  JOB -> Subdirectory in which results are to be stored
-%
-%  imap = 0 -> differential equation
-%       = 1 -> iterated map
-%  x0   = initial condition
-%  p0   = parameters
-%  tfinal    = final time
-%  solntimes = times at which the solution is required
-%  ntsteps = 0 -> Matlab solver determines solution times
-%                 (computes FIM as a function of time)
-%          > 1 -> length of solntimes vector (solntimes(1)=0, the initial time)
-%                 (computes FIM based on selected solution times)
-%  qtype   = 0 -> non-differentiable function of (x,p)
-%          = 1 -> differentiable function of (x,p)
-%  stype   = 1 -> solution only
-%          = 2 -> solution and sensitivities wrt parameters
-%          = 3 -> solution and sensitivities wrt parameters and initial conditions
-%
-%  NextGen -> input for R0 calculation
-%  R0_only -> input for R0 calculation
-%
+function [DIR,JOB,imap,x0,p0,tfinal,ntsteps,qtype,stype,NextGen,R0_only]=user_inputs 
 
-DIR = 'C:/CSU/SENSAI_output/HodgkinHuxley';
-JOB = 'HH-4-Jen-May11';
+DIR = '/home/michael/sensai/HodgkinHuxley';
+JOB = 'HH-SpringBreak';
 
 imap = 0;
 
 x0(1,1) = -75;
 x0(2,1) = 0.05;
-x0(3,1) = 0.4;
-x0(4,1) = 0.45;
+x0(3,1) = 0.6;
+x0(4,1) = 0.325;
 
 p0(1,1) = 120;
 p0(2,1) = 36;
 p0(3,1) = 0.3;
 
-tfinal=40;
-solntimes=0;
-ntsteps=0;
-% solntimes=linspace(0,tfinal,1201);
-% ntsteps=length(solntimes);
+tfinal = 30;
+ntsteps = 0;
 
 qtype = 1;
-stype = 2;
+stype = 3;
 
 NextGen(1,1) = 0;
 R0_only = 0;
--- thirdparty/SENSAI_MuPad/examples/ODE_examples/HodgkinHuxley/user_plotdata.m
+++ thirdparty/SENSAI_MuPad/examples/ODE_examples/HodgkinHuxley/user_plotdata.m
@@ -1,13 +1,13 @@
 function [iplot,ilist,klist]=user_plotdata
 
-iplot.sensitivities = 1;
-iplot.elasticities = 0;
+iplot.sensitivities = 0;
+iplot.elasticities = 1;
 iplot.dxdp.true = 0;
-iplot.dxdp.var = 1;
+iplot.dxdp.var = 0;
 iplot.dxdp.param = 0;
 iplot.dxdz = 0;
 iplot.dqdp = 1;
-iplot.dqdz = 1;
+iplot.dqdz = 0;
 iplot.cp = 0;
 
 ilist(1) = 1;
