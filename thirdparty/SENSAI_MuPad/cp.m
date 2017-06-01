f4061b5d95449f8123503318f1ced5d9c8b09f10
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-06-01 09:24:23 +0100

    Ran logistic example.

--- thirdparty/SENSAI_MuPad/cp.m
+++ thirdparty/SENSAI_MuPad/cp.m
@@ -7,17 +7,9 @@ cparam = p(1);
 dcdp1 = 1.0;
 dcdp2 = 0.0;
 dcdp3 = 0.0;
-dcdp4 = 0.0;
-dcdp5 = 0.0;
-dcdp6 = 0.0;
-dcdp7 = 0.0;
 
 dcdp(1) = dcdp1;
 dcdp(2) = dcdp2;
 dcdp(3) = dcdp3;
-dcdp(4) = dcdp4;
-dcdp(5) = dcdp5;
-dcdp(6) = dcdp6;
-dcdp(7) = dcdp7;
 
 end
