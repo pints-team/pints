7a29410e4c4303d3dd28a587e96581567b706588
Michael Clerx (UM) <Michael.Clerx@maastrichtuniversity.nl>
2017-06-07 12:59:29 +0100

    Added simple csv loader in free/aslanidi

--- free/stan-ikr/aslanidi/run.py
+++ free/stan-ikr/aslanidi/run.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 import numpy as np
-import myokit
 import matplotlib.pyplot as pl
 import pystan
+import csvload
 
-d = myokit.DataLog.load_csv('simple.csv').npview()
-
+# Load data
+d = csvload.load('simple.csv')
 t = d['engine.time']
 V = d['membrane.V']
 I = d['ikr.IKr']
 
-if False:
+# Show data
+if True:
     pl.figure()
     pl.subplot(2,1,1)
     pl.plot(t, V)
@@ -18,4 +19,5 @@ if False:
     pl.plot(t, I)
     pl.show()
 
+# Show stan file
 model = pystan.StanModel('aslanidi-2009.stan')
