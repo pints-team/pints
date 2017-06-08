efc5cf1c32fb2a9f605f6d6e4cfb96f096f99e20
Michael Clerx (UM) <Michael.Clerx@maastrichtuniversity.nl>
2017-06-08 14:41:38 +0100

    Added myokit in 'free' directory

---
+++ free/myokit/myokit/formats/latex/__init__.py
@@ -0,0 +1,33 @@
+#
+# Provides latex export
+#
+# This file is part of Myokit
+#  Copyright 2011-2016 Michael Clerx, Maastricht University
+#  Licensed under the GNU General Public License v3.0
+#  See: http://myokit.org
+#
+# Exporters
+from _exporter import PdfExporter, PosterExporter
+_exporters = {
+    'latex-article' : PdfExporter,
+    'latex-poster' : PosterExporter,
+    }
+def exporters():
+    """
+    Returns a dict of all exporters available in this module.
+    """
+    return dict(_exporters)
+# Expression writers
+from _ewriter import LatexExpressionWriter
+_ewriters = {
+    'latex' : LatexExpressionWriter,
+    }
+def ewriters():
+    """
+    Returns a dict of all expression writers available in this module.
+    """
+    return dict(_ewriters)
+#
+# Language keywords
+#
+# None!
