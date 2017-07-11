d60d07c34a45d264f840d31fb2a38de719fcd880
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-07-11 16:45:50 +0100

    Kylie: Toying with cmaes

--- free/myokit/myokit/__init__.py
+++ free/myokit/myokit/__init__.py
@@ -47,7 +47,7 @@ if sys.hexversion > 0x03000000:
 del(sys)
 # Constants
 # Version information
-VERSION_INT = 1,25,0
+VERSION_INT = 0,0,0
 VERSION = '.'.join([str(x) for x in VERSION_INT]); del(x)
 RELEASE = ''
 # Licensing
