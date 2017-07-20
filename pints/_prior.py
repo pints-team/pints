044bab3f518d54196f9c8eaf2b8589793674ac06
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-07-20 10:20:27 +0100

    Added license info, organising python code.

--- pints/_prior.py
+++ pints/_prior.py
@@ -1,3 +1,11 @@
+#
+# Defines different prior distributions
+#
+# This file is part of PINTS.
+#  Copyright (c) 2017, University of Oxford.
+#  For licensing information, see the LICENSE file distributed with the PINTS
+#  software package.
+#
 import numpy as np
 
 class Uniform:
