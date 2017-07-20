97b9a18286ddfcd1ac18021a4258c00b207626ce
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-07-20 10:43:28 +0100

    Added python unit testing code

---
+++ run-tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Runs all unit tests included in PINTS.
+#
+# This file is part of PINTS.
+#  Copyright (c) 2017, University of Oxford.
+#  For licensing information, see the LICENSE file distributed with the PINTS
+#  software package.
+#
+python -m unittest discover -v test
