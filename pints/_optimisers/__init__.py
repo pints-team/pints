595941485054f10241db769adf1bcae8cda30e7e
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-07-25 20:53:37 +0100

    Moved optimisers into separate module (but still get imported into main package)

---
+++ pints/_optimisers/__init__.py
@@ -0,0 +1,10 @@
+#
+# Sub-module containing several optimisation routines
+#
+# This file is part of PINTS.
+#  Copyright (c) 2017, University of Oxford.
+#  For licensing information, see the LICENSE file distributed with the PINTS
+#  software package.
+#
+
+
