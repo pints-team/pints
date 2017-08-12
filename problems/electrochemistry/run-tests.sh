1ca340336af7ddb81595912b1efa6e7f2dd6088f
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-08-12 14:00:19 +0100

    Reworking electrochemistry tests

--- problems/electrochemistry/run-tests.sh
+++ problems/electrochemistry/run-tests.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+#
+# Runs the electrochemistry tests
+#
+# This file is part of PINTS.
+#  Copyright (c) 2017, University of Oxford.
+#  For licensing information, see the LICENSE file distributed with the PINTS
+#  software package.
+#
 PINTS=$(pwd)/../../
 BUILD=$(pwd)/build/
-env PYTHONPATH=$PYTHONPATH:$PINTS:$BUILD python2 tests/tests.py
+PPATH=$PYTHONPATH:$PINTS:$BUILD
+env PYTHONPATH=$PPATH python -m unittest discover -v test
