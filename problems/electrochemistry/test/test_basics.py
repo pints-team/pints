9afe1eb30dd754f2fc932d68d0596e394c4a90c0
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-08-15 13:45:20 +0100

    Tidied echem tests

--- problems/electrochemistry/test/test_basics.py
+++ problems/electrochemistry/test/test_basics.py
@@ -6,8 +6,4 @@ class TestBasics(unittest.TestCase):
     def test_import(self):
         import pints
         import electrochemistry
-        #self.assertEqual(1, 2)
-    def test_hello(self):
-        print('HELLO')
-        self.assertEqual(3, 4)
         
