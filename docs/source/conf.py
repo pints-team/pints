7e0d0d88db099adcc9f38e7480af6a25dfc7fa3f
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-11-22 10:57:17 +0000

    Updating docs to work with readthedocs.org

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -108,19 +108,19 @@ html_static_path = ['_static']
 
 # Guzzle theme
 
-import guzzle_sphinx_theme
+#import guzzle_sphinx_theme
 
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
+#html_theme_path = guzzle_sphinx_theme.html_theme_path()
+#html_theme = 'guzzle_sphinx_theme'
 
 # Register the theme as an extension to generate a sitemap.xml
-extensions.append("guzzle_sphinx_theme")
+#extensions.append("guzzle_sphinx_theme")
 
 # Guzzle theme options (see theme.conf for more information)
-html_theme_options = {
-    # Set the name of the project to appear in the sidebar
-    "project_nav_name": project,
-}
+#html_theme_options = {
+#    # Set the name of the project to appear in the sidebar
+#    "project_nav_name": project,
+#}
 
 # -- Options for HTMLHelp output ------------------------------------------
 
