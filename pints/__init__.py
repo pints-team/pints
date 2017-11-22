6edeadf1e228ded809648e692ec66d9b144a78c1
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-11-22 11:10:41 +0000

    Removed old plotting code

--- pints/__init__.py
+++ pints/__init__.py
@@ -80,9 +80,3 @@ from _optimisers._xnes import XNES, xnes
 from _mcmc import MCMC
 from _mcmc._adaptive import AdaptiveCovarianceMCMC, adaptive_covariance_mcmc
 
-#
-#TODO
-#
-#from _mcmc import hierarchical_gibbs_sampler
-#from _plot import scatter_grid,plot_trace,scatter_diagonal
-
