96cdcb5a7f63424b2ad9d6b4d87d352d1f06dd63
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-12-07 16:13:49 +0000

    Added methods to set PSO stopping criteria, including tests.

--- pints/_util.py
+++ pints/_util.py
@@ -6,8 +6,10 @@
 #  For licensing information, see the LICENSE file distributed with the PINTS
 #  software package.
 #
+from cStringIO import StringIO
 import pints
 import numpy as np
+import sys
 
 def strfloat(x):
     """
@@ -29,3 +31,66 @@ def vector(x):
             raise ValueError('Unable to convert to 1d vector of scalar values')
         x = x.reshape((n,))
     return x
+
+class _StdOutCapture(object):
+    """
+    A context manager that redirects and captures the standard output of the
+    python interpreter.
+    """
+    # Based on myokit PyCapture object
+    def __init__(self, enabled=True):
+        super(_StdOutCapture, self).__init__()
+        self._capturing = False     # True if currently capturing
+        self._captured = []         # Array to store captured strings in
+        self._stdout = None     # Original stdout
+        self._dupout = None     # String buffer to redirect stdout to
+
+    def __enter__(self):
+        """ Called when the context is entered. """
+        self._start_capturing()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Called when exiting the context.
+        """
+        self._stop_capturing()
+
+    def _start_capturing(self):
+        """ Starts capturing output. """
+        if not self._capturing:
+            # If possible, flush current outputs
+            try:
+                sys.stdout.flush()
+            except AttributeError:
+                pass
+            # Save current sys stdout
+            self._stdout = sys.stdout
+            # Create temporary buffers
+            self._dupout = StringIO()
+            # Re-route
+            sys.stdout = self._dupout
+            # Now we're capturing!
+            self._capturing = True
+    
+    def _stop_capturing(self):
+        """ Stops capturing output. """
+        if self._capturing:
+            # Flush any remaining output to streams
+            self._dupout.flush()
+            # Restore original stdout and stderr
+            sys.stdout = self._stdout
+            # Get captured output
+            self._captured.append(self._dupout.getvalue())
+            # Delete buffer
+            self._dupout = None
+            # No longer capturing
+            self._capturing = False
+
+    def text(self):
+        """
+        Disables capturing and returns the captured text.
+        """
+        self._stop_capturing()
+        return ''.join(self._captured)
+
