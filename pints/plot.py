9ca543a37415e8bef3fa9b7c1be2c7f5f55173b6
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-12-11 15:30:54 +0000

    Final style fixes, code is now OK according to flak8 (with the exceptions defined in .flake8).

--- pints/plot.py
+++ pints/plot.py
@@ -7,6 +7,9 @@
 #  software package.
 #
 from __future__ import division
+import numpy as np
+from scipy import stats
+
 
 def trace(chain, *args):
     """
@@ -24,27 +27,24 @@ def trace(chain, *args):
 
     Returns a `matplotlib` figure object and axes handle.
     """
-    try:
-        import matplotlib.pyplot as plt
-    except:
-        raise ImportError("The method pints.plot.trace requires matplotlib")
+    import matplotlib.pyplot as plt
     
     bins = 40
     alpha = 0.5
     n_sample, n_param = chain.shape
 
     # Set up figure, plot first chain
-    fig, axes = plt.subplots(n_param, 2, figsize=(12, 2*n_param))
+    fig, axes = plt.subplots(n_param, 2, figsize=(12, 2 * n_param))
     for i in xrange(n_param):
         # Add histogram subplot
         axes[i, 0].set_xlabel('Parameter ' + str(i + 1))
         axes[i, 0].set_ylabel('Frequency')
-        axes[i, 0].hist(chain[:,i], bins=bins, alpha=0.5, label='Chain 1')
+        axes[i, 0].hist(chain[:, i], bins=bins, alpha=0.5, label='Chain 1')
 
         # Add trace subplot
         axes[i, 1].set_xlabel('Iteration')
         axes[i, 1].set_ylabel('Parameter ' + str(i + 1))
-        axes[i, 1].plot(chain[:,i], alpha=alpha)
+        axes[i, 1].plot(chain[:, i], alpha=alpha)
 
     # Plot additional chains
     if args:
@@ -53,9 +53,9 @@ def trace(chain, *args):
                 raise ValueError('All chains must have the same number of'
                     ' parameters.')
             for i in xrange(n_param):
-                axes[i, 0].hist(chain[:,i], bins=bins, alpha=0.5, 
+                axes[i, 0].hist(chain[:, i], bins=bins, alpha=0.5,
                                 label='Chain ' + str(2 + i_chain))
-                axes[i, 1].plot(chain[:,i], alpha=alpha)
+                axes[i, 1].plot(chain[:, i], alpha=alpha)
         axes[0, 0].legend()
 
     plt.tight_layout()
@@ -77,26 +77,22 @@ def autocorrelation(chain, max_lags=100):
 
     Returns a `matplotlib` figure object and axes handle.
     """
-    import numpy as np
-    try:
-        import matplotlib.pyplot as plt
-    except:
-        raise ImportError("The method pints.plot.autocorrelation requires matplotlib")
+    import matplotlib.pyplot as plt
     
     n_sample, n_param = chain.shape
 
-    fig, axes = plt.subplots(n_param, 1, sharex=True, figsize=(6, 2*n_param))
+    fig, axes = plt.subplots(n_param, 1, sharex=True, figsize=(6, 2 * n_param))
     for i in xrange(n_param):
-        axes[i].acorr(chain[:,i] - np.mean(chain[:,i]), maxlags=max_lags)
-        axes[i].set_xlim(-0.5, max_lags+0.5)
+        axes[i].acorr(chain[:, i] - np.mean(chain[:, i]), maxlags=max_lags)
+        axes[i].set_xlim(-0.5, max_lags + 0.5)
         axes[i].legend(['Parameter ' + str(1 + i)], loc='upper right')
     
     # Add x-label to final plot only
     axes[i].set_xlabel('Lag')
     
     # Add vertical y-label to middle plot
-    #fig.text(0.04, 0.5, 'Autocorrelation', va='center', rotation='vertical')
-    axes[int(i/2)].set_ylabel('Autocorrelation')
+    # fig.text(0.04, 0.5, 'Autocorrelation', va='center', rotation='vertical')
+    axes[int(i / 2)].set_ylabel('Autocorrelation')
 
     plt.tight_layout()
     return fig, axes
@@ -106,7 +102,7 @@ def series(chain, problem, thinning=None):
     """
     Creates and returns a plot of predicted time series for a given markov
     `chain` and a single-series `problem`.
-    
+
     Because this method runs simulations, it can take a considerable time to
     run.
 
@@ -122,18 +118,14 @@ def series(chain, problem, thinning=None):
         in the chain but not accepted by the SingleSeriesProblem (for example
         parameters added by a noise model) will be ignored.
     `thinning`
-        (Optional) An integer greater than zero. If specified, only every 
+        (Optional) An integer greater than zero. If specified, only every
         n-th sample (with `n = thinning`) in the chain will be used. If left at
         the default value `None`, a value will be chosen so that 200 to 400
         predictions are shown.
 
     Returns a `matplotlib` figure object and axes handle.
     """
-    import numpy as np
-    try:
-        import matplotlib.pyplot as plt
-    except:
-        raise ImportError("The method pints.plot.series requires matplotlib")
+    import matplotlib.pyplot as plt
     
     n_sample, n_param = chain.shape
 
@@ -149,7 +141,7 @@ def series(chain, problem, thinning=None):
             raise ValueError('Thinning rate must be `None` or an integer'
                 ' greater than zero.')
 
-    # Get times    
+    # Get times
     times = problem.times()
 
     # Evaluate the model for all parameter sets in the chain
@@ -201,15 +193,10 @@ def pairwise(chain, kde=False):
 
     Returns a `matplotlib` figure object and axes handle.
     """
-    import numpy as np
-    from scipy import stats
-    try:
-        import matplotlib.pyplot as plt
-    except:
-        raise ImportError("The method pints.plot.pairwise requires matplotlib")
+    import matplotlib.pyplot as plt
     
     n_sample, n_param = chain.shape
-    fig_size = (3*n_param, 3*n_param)
+    fig_size = (3 * n_param, 3 * n_param)
 
     bins = 25
     fig, axes = plt.subplots(n_param, n_param, figsize=fig_size)
@@ -217,59 +204,59 @@ def pairwise(chain, kde=False):
         for j in range(n_param):
             if i == j:
                 # Diagonal: Plot a histogram
-                xmin, xmax = np.min(chain[:,i]), np.max(chain[:,i])
+                xmin, xmax = np.min(chain[:, i]), np.max(chain[:, i])
                 xbins = np.linspace(xmin, xmax, bins)
-                axes[i,j].hist(chain[:,i], bins=xbins, normed=True)
+                axes[i, j].hist(chain[:, i], bins=xbins, normed=True)
                 if kde:
                     x = np.linspace(xmin, xmax, 100)
-                    axes[i,j].plot(x, stats.gaussian_kde(chain[:,i])(x))
+                    axes[i, j].plot(x, stats.gaussian_kde(chain[:, i])(x))
             elif i < j:
                 # Top-right: no plot
-                axes[i,j].axis('off')
+                axes[i, j].axis('off')
             else:
                 # Lower-left: Plot the samples as density map
-                xmin, xmax = np.min(chain[:,j]), np.max(chain[:,j])
-                ymin, ymax = np.min(chain[:,i]), np.max(chain[:,i])
+                xmin, xmax = np.min(chain[:, j]), np.max(chain[:, j])
+                ymin, ymax = np.min(chain[:, i]), np.max(chain[:, i])
                 if not kde:
                     # Create an ordinary histogram
                     xbins = np.linspace(xmin, xmax, bins)
                     ybins = np.linspace(ymin, ymax, bins)
-                    axes[i,j].hist2d(chain[:,j], chain[:,i],
-                        bins=[xbins,ybins], normed=True, cmap=plt.cm.Blues)
+                    axes[i, j].hist2d(chain[:, j], chain[:, i],
+                        bins=[xbins, ybins], normed=True, cmap=plt.cm.Blues)
                 else:
                     # Create a kernel-density estimate plot
                     x, y = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
-                    values = np.vstack([chain[:,j], chain[:,i]])
+                    values = np.vstack([chain[:, j], chain[:, i]])
                     values = stats.gaussian_kde(values)
                     values = values(np.vstack([x.ravel(), y.ravel()]))
                     values = np.reshape(values.T, x.shape)
-                    axes[i,j].imshow(np.rot90(values), cmap=plt.cm.Blues,
+                    axes[i, j].imshow(np.rot90(values), cmap=plt.cm.Blues,
                         extent=[xmin, xmax, ymin, ymax])
-                    axes[i,j].plot(chain[:,j], chain[:,i], 'k.', markersize=2,
-                        alpha=0.5)
+                    axes[i, j].plot(chain[:, j], chain[:, i], 'k.',
+                        markersize=2, alpha=0.5)
                 
                     # Force equal aspect ratio
                     # See: https://stackoverflow.com/questions/7965743
-                    im = axes[i,j].get_images()
+                    im = axes[i, j].get_images()
                     ex = im[0].get_extent()
-                    axes[i,j].set_aspect(
+                    axes[i, j].set_aspect(
                         abs((ex[1] - ex[0]) / (ex[3] - ex[2])))
                 
-            if i < n_param-1:
+            if i < n_param - 1:
                 # Only show x tick labels for the last row
-                axes[i,j].set_xticklabels([])
+                axes[i, j].set_xticklabels([])
             else:
                 # Rotate the x tick labels to fit in the plot
-                for tl in axes[i,j].get_xticklabels():
+                for tl in axes[i, j].get_xticklabels():
                     tl.set_rotation(45)
             if j > 0:
                 # Only show y tick labels for the first column
-                axes[i,j].set_yticklabels([])
+                axes[i, j].set_yticklabels([])
         if i > 0:
             # The first one is not a parameter
-            axes[i,0].set_ylabel('Parameter %d'%(i+1))
+            axes[i, 0].set_ylabel('Parameter %d' % (i + 1))
         else:
-            axes[i,0].set_ylabel('Frequency')
-        axes[-1,i].set_xlabel('Parameter %d'%(i+1))
+            axes[i, 0].set_ylabel('Frequency')
+        axes[-1, i].set_xlabel('Parameter %d' % (i + 1))
 
     return fig, axes
