2fbcec851387a1f871c0ed1b4f51a37e3a941531
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-12-18 16:24:57 +0000

    Updated shebangs in tests. Closes #145

--- test/test_likelihood.py
+++ test/test_likelihood.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Tests the log likelihood methods
 #
--- test/test_logistic_model.py
+++ test/test_logistic_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Tests if the logistic (toy) model works.
 #
