4b4f265318808f99cf944d66d782b77e19cd2780
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-12-19 12:35:29 +0000

    Style updates

--- pints/_diagnostics.py
+++ pints/_diagnostics.py
@@ -5,46 +5,56 @@
 #  Copyright (c) 2017, University of Oxford.
 #  For licensing information, see the LICENSE file distributed with the PINTS
 #  software package.
-import pints
 import numpy as np
 
-## calculates autocorrelation function for a vector x by spectra density calculation
-def Autocorrelation(lx) :
-    xp = lx-np.mean(lx)
-    f = np.fft.fft(xp)
-    p = np.array([np.real(v)**2+np.imag(v)**2 for v in f])
-    pi = np.fft.ifft(p)
-    return np.real(pi)[:lx.size/2]/np.sum(xp**2)
 
-## finds last positive autocorrelation, T
-def AutocorrelateNegative(lAutocorrelation):
+def autocorrelation(lx):
+    """
+    Calculate autocorrelation for a vector x using a spectrum density
+    calculation.
+    """
+    xp = lx - np.mean(lx)
+    f = np.fft.fft(xp)
+    p = np.array([np.real(v)**2 + np.imag(v)**2 for v in f])
+    pi = np.fft.ifft(p)
+    return np.real(pi)[:lx.size / 2] / np.sum(xp**2)
+
+
+def autocorrelate_negative(l_autocorrelation):
+    """
+    ## finds last positive autocorrelation, T
+    """
     T = 1
-    for a in lAutocorrelation:
+    for a in l_autocorrelation:
         if a < 0:
             return T - 1
         T += 1
     return -1
 
-## calculates ESS for a single parameter
-def ESS_singleParam(lx):
-    lRho = Autocorrelation(lx)
-    T = AutocorrelateNegative(lRho)
-    n = len(lx)
-    ESS = n / (1 + 2 * np.sum(lRho[0:(T-1)]))
-    return ESS
 
-## calculates ESS for a matrix of samples
-def EffectiveSampleSize(mSample):
-    try: 
-        nSample, nParams = mSample.shape
+def ess_single_param(lx):
+    """
+    ## calculates ESS for a single parameter
+    """
+    l_rho = autocorrelation(lx)
+    T = autocorrelate_negative(l_rho)
+    n = len(lx)
+    ess = n / (1 + 2 * np.sum(l_rho[0:(T - 1)]))
+    return ess
+
+
+def effective_sample_size(m_sample):
+    """
+    ## calculates ESS for a matrix of samples
+    """
+    try:
+        n_sample, n_params = m_sample.shape
     except IndexError:
         IndexError('There must be at least one parameter')
-    assert nSample > 1
-    
-    ESS = np.zeros(nParams)
-    for i in range(0,nParams):
-        ESS[i] = ESS_singleParam(mSample[:,i])
-    return ESS
-    
-    
-    
+    assert n_sample > 1
+
+    ess = np.zeros(n_params)
+    for i in range(0, n_params):
+        ess[i] = ess_single_param(m_sample[:, i])
+    return ess
+
