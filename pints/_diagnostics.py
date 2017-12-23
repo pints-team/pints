88d33c7c896f3326382c63263037c9398e246b88
Ben Lambert <ben.c.lambert@gmail.com>
2017-12-23 13:37:21 +0000

    Style changes to _diagnostics.
    Added a little text to inference-adaptive-covariance example, re: the effective sample size.

--- pints/_diagnostics.py
+++ pints/_diagnostics.py
@@ -8,53 +8,53 @@
 import numpy as np
 
 
-def autocorrelation(lx):
+def autocorrelation(x):
     """
     Calculate autocorrelation for a vector x using a spectrum density
     calculation.
     """
-    xp = lx - np.mean(lx)
+    xp = x - np.mean(x)
     f = np.fft.fft(xp)
     p = np.array([np.real(v)**2 + np.imag(v)**2 for v in f])
     pi = np.fft.ifft(p)
-    return np.real(pi)[:lx.size / 2] / np.sum(xp**2)
+    return np.real(pi)[:x.size / 2] / np.sum(xp**2)
 
 
-def autocorrelate_negative(l_autocorrelation):
+def autocorrelate_negative(autocorrelation):
     """
     ## finds last positive autocorrelation, T
     """
     T = 1
-    for a in l_autocorrelation:
+    for a in autocorrelation:
         if a < 0:
             return T - 1
         T += 1
     return -1
 
 
-def ess_single_param(lx):
+def ess_single_param(x):
     """
     ## calculates ESS for a single parameter
     """
-    l_rho = autocorrelation(lx)
-    T = autocorrelate_negative(l_rho)
-    n = len(lx)
-    ess = n / (1 + 2 * np.sum(l_rho[0:(T - 1)]))
+    rho = autocorrelation(x)
+    T = autocorrelate_negative(rho)
+    n = len(x)
+    ess = n / (1 + 2 * np.sum(rho[0:(T - 1)]))
     return ess
 
 
-def effective_sample_size(m_sample):
+def effective_sample_size(sample):
     """
     ## calculates ESS for a matrix of samples
     """
     try:
-        n_sample, n_params = m_sample.shape
+        n_sample, n_params = sample.shape
     except IndexError:
         IndexError('There must be at least one parameter')
     assert n_sample > 1
 
     ess = np.zeros(n_params)
     for i in range(0, n_params):
-        ess[i] = ess_single_param(m_sample[:, i])
+        ess[i] = ess_single_param(sample[:, i])
     return ess
 
