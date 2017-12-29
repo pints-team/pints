8ed7fb2432c01dd274cbbc0c50b39cdfb0cc5695
ben18785 <ben.c.lambert@gmail.com>
2017-12-29 12:53:04 +0000

    All toy pdfs appear to be working.
    Also created a toy pdf Jupyter notebook which illustrates how to call these types of object and run sampling on them.

--- pints/toy/_logpdfs.py
+++ pints/toy/_logpdfs.py
@@ -51,7 +51,7 @@ class TwistedGaussianLogPDF(LogPDF):
     International Journal of Nonlinear Sciences and Numerical
     Simulation.
     """
-    def __init__(self, b):
+    def __init__(self, b=0.1):
         
         super(TwistedGaussianLogPDF, self).__init__(10)
         self._b = float(b)
@@ -59,11 +59,14 @@ class TwistedGaussianLogPDF(LogPDF):
             raise ValueError('b cannot be negative.')
 
     def __call__(self, x):
+        if len(x) != 10:
+            raise ValueError('Dimensions must equal 10')
         cov = np.diag(np.repeat(1, 10))
         cov[0,0] = 100
-        mu = np.array([x[0], x[1] + self._b * x[0]**2 - 100 * self._b,
+        mu = np.repeat(0, 10)
+        phi = np.array([x[0], x[1] + self._b * x[0]**2 - 100 * self._b,
                        x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9]])
-        log_pdf = multivariate_normal.logpdf(x, mean=mu, cov=cov)
+        log_pdf = multivariate_normal.logpdf(phi, mean=mu, cov=cov)
         return log_pdf
 
 
@@ -75,47 +78,83 @@ class MultivariateGaussianLogPDF(LogPDF):
     Multivariate Gaussian pdf. Default is a covariance
     matrix with cor(i,j) = 0.5, and var(i) = i.
     """
-    def __init__(self, dimensions, mu, cov=None):
-      
+    def __init__(self, dimensions=100, mu=None, cov=None):
+        super(MultivariateGaussianLogPDF, self).__init__(dimensions)
+        
         if dimensions < 1:
             raise ValueError('Dimensions must equal or exceed 1.')
-        if len(mu)!= dimensions:
-            raise ValueError('Length of mean must equal specified dimensions.')
-        
-        
-        super(MultivariateGaussianLogPDF, self).__init__(dimensions)
+        if mu is not None:
+            if len(mu)!= dimensions:
+                raise ValueError('Length of mean must equal specified dimensions.')
+        if cov is not None:
+            dim1, dim2 = cov.shape
+            if dim1 != dim2:
+                raise ValueError('Covariance matrix must be square')
+            if dim1 != dimensions or dim2 != dimensions:
+                raise ValueError('Dimensions of covariance matrix must match pdf dimensions (default is 100).')
+                
+        # Construct covariance matrix where diagonal variances = j
+        # and off-diagonal covariances = 0.5 * sqrt(i) * sqrt(j)
+        if cov is None:
+          cor = 0.5 * np.ones((dimensions, dimensions))
+          np.fill_diagonal(cor, 1)
+          cov = cor
+          for i in range(0, dimensions):
+              for j in range(0, dimensions):
+                  cov[i,j] *= np.sqrt(i + 1) * np.sqrt(j + 1)
+        self._cov = cov
+        if mu is None:
+            mu = np.repeat(0, dimensions)
+        self._mu = mu
 
     def __call__(self, x):
-        cov = np.diag(np.repeat(1, 10))
-        cov[0,0] = 100
-        mu = np.array([x[0], x[1] + self._b * x[0]**2 - 100 * self._b,
-                       x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9]])
-        log_pdf = multivariate_normal.logpdf(x, mean=mu, cov=cov)
+        if len(x) != self._dimension:
+            raise ValueError('Dimensions of x must equal pdf dimensions.')
+        log_pdf = multivariate_normal.logpdf(x, mean=self._mu, cov=self._cov)
         return log_pdf
 
 
-class BimodalGaussianLogPDF(LogPDF):
+class BimodalMultivariateGaussianLogPDF(LogPDF):
     """
     *Extends:* :class:`LogPDF`
     
-    Multivariate Gaussian pdf. Default is a covariance
-    matrix with cor(i,j) = 0.5, and var(i) = i.
+    Bimodal multivariate (un-normalised) Gaussian. Default is 2D with modes
+    at (0,0) and (10,10) with independent unit 
+    covariance matrices.
     """
-    def __init__(self, mu1, mu2, cov1=None, cov2=None):
-      
-        if len(mu1)!= 2:
-            raise ValueError('Length of mean must equal 2.')
-        if len(mu2)!= 2:
-            raise ValueError('Length of mean must equal 2.')
-
-        super(BimodalGaussianLogPDF, self).__init__(2)
+    def __init__(self, dimensions=2, mu1=[0,0], mu2=[10,10], cov1=None, cov2=None):
+        super(BimodalMultivariateGaussianLogPDF, self).__init__(dimensions)
+        if len(mu1)!= dimensions:
+            raise ValueError('Length of mean must equal pdf dimensions (default is 2).')
+        if len(mu2)!= dimensions:
+            raise ValueError('Length of mean must equal pdf dimensions (default is 2).')
+        if cov1 is None:
+            cov1 = np.diag(np.repeat(1, dimensions))
+        else:
+            dim1, dim2 = cov1.shape
+            if dim1 != dim2:
+                raise ValueError('Covariance matrix must be square')
+            if dim1 != dimensions or dim2 != dimensions:
+                raise ValueError('Dimensions of covariance matrix must match pdf dimensions (default is 2).')
+        if cov2 is None:
+            cov1 = np.diag(np.repeat(1, dimensions))
+        else:    
+            dim1, dim2 = cov2.shape
+            if dim1 != dim2:
+                raise ValueError('Covariance matrix must be square')
+            if dim1 != dimensions or dim2 != dimensions:
+                raise ValueError('Dimensions of covariance matrix must match pdf dimensions (default is 2).')
+        self._mu1 = mu1
+        self._mu2 = mu2
+        self._cov1 = cov1
+        self._cov2 = cov2
+        
 
     def __call__(self, x):
-        cov = np.diag(np.repeat(1, 10))
-        cov[0,0] = 100
-        mu = np.array([x[0], x[1] + self._b * x[0]**2 - 100 * self._b,
-                       x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9]])
-        log_pdf = multivariate_normal.logpdf(x, mean=mu, cov=cov)
+        if len(x) != self._dimension:
+            raise ValueError('Dimensions of x must equal pdf dimensions.')
+        log_pdf = np.log(multivariate_normal.pdf(x, mean=self._mu1, cov=self._cov1) +
+                         multivariate_normal.pdf(x, mean=self._mu2, cov=self._cov2))
         return log_pdf
 
 
@@ -125,12 +164,15 @@ class RosenbrockLogPDF(LogPDF):
     
     Rosenbrock function (see: https://en.wikipedia.org/wiki/Rosenbrock_function):
     
-    f(x,y) = (a - x)^2 + b(y - x^2)^2
+    f(x,y) = -((a - x)^2 + b(y - x^2)^2)
+    
+    Note the minus sign converts this from a minimisation to a maximisation
+    problem.
     """
-    def __init__(self, a, b):
+    def __init__(self, a=1, b=100):
         self._a = a
         self._b = b
         super(RosenbrockLogPDF, self).__init__(2)
 
     def __call__(self, x):
-        return np.log((self._a - x[0])**2 + self._b * (x[1] - x[0]**2)**2)
+        return - np.log((self._a - x[0])**2 + self._b * (x[1] - x[0]**2)**2)
