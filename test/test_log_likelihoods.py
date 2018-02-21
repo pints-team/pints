Added SumOfIndependentLogLikelihoods class. Fix WeightedSumOfErrors tests. Separated ProblemLogLikelihood from LogLikelihood. Closes #231. Closes #240.

test/test_log_likelihoods.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 #
-# Tests the log likelihood methods
+# Tests the log likelihood classes.
 #
 # This file is part of PINTS.
 #  Copyright (c) 2017-2018, University of Oxford.
 #  For licensing information, see the LICENSE file distributed with the PINTS
 #  software package.
 #
-# TODO Add tests for the remaining likelihood classes and methods
 import unittest
 import pints
 import pints.toy as toy
@@ -39,7 +38,7 @@ class TestLogLikelihood(unittest.TestCase):
         self.assertEqual(int(eval_not_scaled), -20959169232)
         self.assertAlmostEqual(eval_scaled * len(times), eval_not_scaled)
 
-    def test_known_unknown_log_likelihood(self):
+    def test_known_and_unknown_noise_log_likelihood(self):
 
         model = toy.LogisticModel()
         parameters = [0.015, 500]
@@ -52,6 +51,32 @@ class TestLogLikelihood(unittest.TestCase):
         l1 = pints.KnownNoiseLogLikelihood(problem, sigma)
         l2 = pints.UnknownNoiseLogLikelihood(problem)
         self.assertAlmostEqual(l1(parameters), l2(parameters + [sigma]))
+
+    def test_sum_of_independent_log_likelihoods(self):
+        model = toy.LogisticModel()
+        x = [0.015, 500]
+        sigma = 0.1
+        times = np.linspace(0, 1000, 100)
+        values = model.simulate(x, times)
+        problem = pints.SingleSeriesProblem(model, times, values)
+
+        l1 = pints.KnownNoiseLogLikelihood(problem, sigma)
+        l2 = pints.UnknownNoiseLogLikelihood(problem)
+        ll = pints.SumOfIndependentLogLikelihoods(l1, l1, l1)
+        self.assertEqual(3 * l1(x), ll(x))
+
+        # Test invalid constructors
+        # Wrong number of arguments
+        self.assertRaises(ValueError, pints.SumOfIndependentLogLikelihoods)
+        self.assertRaises(ValueError, pints.SumOfIndependentLogLikelihoods, l1)
+        # Wrong types
+        self.assertRaises(
+            ValueError, pints.SumOfIndependentLogLikelihoods, l1, 1)
+        self.assertRaises(
+            ValueError, pints.SumOfIndependentLogLikelihoods, problem, l1)
+        # Mismatching dimensions
+        self.assertRaises(
+            ValueError, pints.SumOfIndependentLogLikelihoods, l1, l2)
 
 
 if __name__ == '__main__':