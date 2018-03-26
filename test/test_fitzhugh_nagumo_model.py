a0deef729dcee83e7ad86df889fb930d532b71c0
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-03-26 18:07:52 +0100

    [multitrace] Added basic test for Fitzhugh-Nagumo model.

---
+++ test/test_fitzhugh_nagumo_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+#
+# Tests if the Fitzhugh-Nagumo toy model runs.
+#
+# This file is part of PINTS.
+#  Copyright (c) 2017-2018, University of Oxford.
+#  For licensing information, see the LICENSE file distributed with the PINTS
+#  software package.
+#
+import unittest
+import pints
+import pints.toy
+
+
+class TestFitzhughNagumoModel(unittest.TestCase):
+    """
+    Tests if the Fitzhugh-Nagumo toy model runs.
+    """
+
+    def test_run(self):
+
+        model = pints.toy.FitzhughNagumoModel()
+        self.assertEqual(model.dimension(), 3)
+        self.assertEqual(model.n_outputs(), 2)
+
+        x = [1, 1, 1]
+        times = [1, 2, 3, 4]
+        values = model.simulate(x, times)
+        self.assertEqual(values.shape, (4, 2))
+
+        # Test alternative starting position
+        model = pints.toy.FitzhughNagumoModel([0.1, 0.1])
+        values = model.simulate(x, times)
+        self.assertEqual(values.shape, (4, 2))
+
+        # Test errors
+        times = [-1, 2, 3, 4]
+        self.assertRaises(ValueError, model.simulate, x, times)
+
+if __name__ == '__main__':
+    unittest.main()
