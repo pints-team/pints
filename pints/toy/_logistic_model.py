35645a20e3677e6af34a5de3b8870ee4667518a9
Martin Robinson <martinjrobins@gmail.com>
2018-03-28 11:06:03 +0100

    #298 added sensitivities for logistic model

--- pints/toy/_logistic_model.py
+++ pints/toy/_logistic_model.py
@@ -16,8 +16,19 @@ class LogisticModel(pints.ForwardModel):
     """
     Logistic model.
 
-    Has two parameters: A growth rate ``r`` and a carrying capacity ``k``.
+    .. math::
+        f(t) &= \\frac{k}{1+(k/p_0 - 1)*\exp(-r t)} \\\\
+        \\frac{df(t)}{dr} &= \\frac{k t (k / p_0 - 1) \exp(-r t)}
+                                   {((k/p_0-1) \exp(-r t) + 1)^2} \\\\
+        \\frac{df(t)}{dk} &= \\frac{k \exp(-r t)}
+                                   {p_0 ((k/p_0-1)\exp(-r t) + 1)^2} 
+                             + \\frac{1}{(k/p_0 - 1)\exp(-r t) + 1}
+
+    Has two parameters: A growth rate :math:`r` and a carrying capacity 
+    :math:`k`. The initial population size :math:`f(0) = p_0` can be set using
+    the (optional) named constructor arg ``initial_population_size``
     """
+
     def __init__(self, initial_population_size=2):
         super(LogisticModel, self).__init__()
         self._p0 = float(initial_population_size)
@@ -37,3 +48,14 @@ class LogisticModel(pints.ForwardModel):
         if k < 0:
             return np.zeros(times.shape)
         return k / (1 + (k / self._p0 - 1) * np.exp(-r * times))
+
+    def sensitivities(self, parameters, times):
+        r, k = [float(x) for x in parameters]
+        t = np.asarray(times)
+        result = np.empty((len(times), len(parameters)))
+        exp = np.exp(-r * t)
+        c = (k / self._p0 - 1)
+        result[:, 0] = k * t * c * exp / (c * exp + 1)**2
+        result[:, 1] = -k * exp / \
+            (self._p0 * (c * exp + 1)**2) + 1 / (c * exp + 1)
+        return result
