dcfb5fdb876355d15a64ae479a09a2a1e474f607
Ben Lambert <ben.c.lambert@gmail.com>
2018-03-30 05:32:35 +0100

    Added tests for noise generators.

--- pints/noise.py
+++ pints/noise.py
@@ -16,6 +16,11 @@ def add_independent_noise(values, sigma):
     Adds independent Gaussian noise (``iid N(0,sigma)``) to a list of simulated
     values.
     """
+    if sigma <= 0:
+        raise ValueError('Standard deviation of noise must be positive')
+    if len(values) < 1:
+        raise ValueError('Must supply at least one value')
+
     return values + np.random.normal(0, sigma, values.shape)
 
 
@@ -26,7 +31,16 @@ def AR1(rho, sigma, T):
     length ``T``. This choice of parameterisation ensures that the AR1 process
     has a mean of 0 and a standard deviation of sigma.
     """
+    if np.absolute(rho) > 1:
+        raise ValueError('rho must be less than 1 in magnitude (otherwise ' +
+                         'the process is explosive)')
+    if sigma <= 0:
+        raise ValueError('Standard deviation of noise must be positive')
+    if T < 1:
+        raise ValueError('Noise must be at least one unit long')
+
     vX = np.zeros(T)
+    vX[0] = np.random.rand()
     for t in range(1, T):
         vX[t] = rho * vX[t - 1] + np.random.normal(
             0, sigma * np.sqrt(1 - rho**2))
@@ -38,7 +52,14 @@ def add_AR1_noise(values, rho, sigma):
     Adds autoregressive order 1 noise to data. i.e. the errors follow
     ``e(t) ~ rho * e(t-1) + v(t)``, where ``v(t) ~ iid N(0,sigma)``.
     """
-    return values + AR1(rho, sigma, values.shape)
+    if np.absolute(rho) > 1:
+        raise ValueError('rho must be less than 1 in magnitude (otherwise '
+                         + 'the process is explosive)')
+    if sigma <= 0:
+        raise ValueError('Standard deviation of noise must be positive')
+    if len(values) < 1:
+        raise ValueError('Must supply at least one value')
+    return values + AR1(rho, sigma, values.shape[0])
 
 
 def AR1_unity(rho, sigma, T):
@@ -49,16 +70,31 @@ def AR1_unity(rho, sigma, T):
     length ``T``. This choice of parameterisation ensures that the AR1 process
     has mean 1 and a standard deviation of sigma.
     """
-    vX = np.zeros(T)
+    if np.absolute(rho) > 1:
+        raise ValueError('rho must be less than 1 in magnitude (otherwise '
+                         + 'the process is explosive)')
+    if sigma <= 0:
+        raise ValueError('Standard deviation of noise must be positive')
+    if T < 1:
+        raise ValueError('Noise must be at least one unit long')
+
+    vX = np.zeros((T + 1))
     vX[0] = 1
-    for t in range(1, T):
+    for t in range(1, T + 1):
         vX[t] = (1 - rho) + rho * vX[t - 1] + np.random.normal(
             0, sigma * np.sqrt(1 - rho**2))
-    return vX
+    return vX[1:]
 
 
 def multiply_AR1_noise(values, rho, sigma):
     """
     Multiplies signal by a noise process that follows an autoregressive order 1
     process of mean 1."""
-    return values * AR1_unity(rho, sigma, values.shape)
+    if np.absolute(rho) > 1:
+        raise ValueError('rho must be less than 1 in magnitude (otherwise ' +
+                         'the process is explosive)')
+    if sigma <= 0:
+        raise ValueError('Standard deviation of noise must be positive')
+    if len(values) < 1:
+        raise ValueError('Must supply at least one value')
+    return values * AR1_unity(rho, sigma, values.shape[0])
