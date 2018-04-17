8a5dd14b0a65315a9253c9098c7d450f2ded613f
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-04-17 09:26:55 +0100

    Cosmetic code change.

--- pints/toy/__init__.py
+++ pints/toy/__init__.py
@@ -9,12 +9,14 @@
 #
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
+
+from ._constant_model import ConstantModel                          # noqa
+from ._fitzhugh_nagumo_model import FitzhughNagumoModel             # noqa
 from ._goodwin_oscillator_model import GoodwinOscillatorModel       # noqa
 from ._hh_ik_model import HodgkinHuxleyIKModel                      # noqa
-from ._fitzhugh_nagumo_model import FitzhughNagumoModel             # noqa
 from ._high_dimensional_normal import HighDimensionalNormalLogPDF   # noqa
 from ._logistic_model import LogisticModel                          # noqa
 from ._multimodal_normal import MultimodalNormalLogPDF              # noqa
 from ._rosenbrock import RosenbrockError, RosenbrockLogPDF          # noqa
 from ._twisted_gaussian_banana import TwistedGaussianLogPDF         # noqa
-from ._constant_model import ConstantModel                          # noqa
+
