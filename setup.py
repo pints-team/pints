4ecf837809f92c494e3311a4cf8d43e6b2e1da26
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-05-31 01:07:17 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ from setuptools import setup, find_packages
 # Load text for description and license
 with open('README.md') as f:
     readme = f.read()
-with open('LICENSE') as f:
-    license = f.read()
 
 # Go!
 setup(
@@ -22,7 +20,7 @@ setup(
     version='0.0.1',
     description='Probabilistic Inference in Noisy Time-Series',
     long_description=readme,
-    license=license,
+    license='BSD 3-clause license',
     # author='',
     # author_email='',
     maintainer='Michael Clerx',
