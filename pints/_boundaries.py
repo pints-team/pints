76caaeb2609d09125c57424da47f53adebfc83ac
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-07-26 13:49:28 +0100

    Updated documentation for Boundaries class and made it possible to sample from LogPDFBoundaries based on a prior.

--- pints/_boundaries.py
+++ pints/_boundaries.py
@@ -32,14 +32,15 @@ class Boundaries(object):
 
     def sample(self, n=1):
         """
-        Returns ``n`` random samples from within the boundaries.
+        Returns ``n`` random samples from within the boundaries, for example to
+        use as starting points for an optimisation.
 
         The returned value is a numpy array with shape ``(n, d)`` where ``n``
         is the requested number of samples, and ``d`` is the dimension of the
         parameter space these boundaries are defined on.
 
-        *Note that the :meth:`sample()` method does not always have to be
-        implemented.*
+        *Note that implementing :meth:`sample()` is optional, so some boundary
+        types may not support it.*
         """
         raise NotImplementedError
 
@@ -147,6 +148,9 @@ class LogPDFBoundaries(Boundaries):
         # Check threshold
         self._threshold = float(threshold)
 
+        # Check if we can sample
+        self._pdf_is_prior = isinstance(log_pdf, pints.LogPrior)
+
     def check(self, parameters):
         """ See :meth:`pints.Boundaries.check()`. """
         return self._log_pdf(parameters) > self._threshold
@@ -154,4 +158,15 @@ class LogPDFBoundaries(Boundaries):
     def n_parameters(self):
         """ See :meth:`pints.Boundaries.n_parameters()`. """
         return self._log_pdf.n_parameters()
+
+    def sample(self, n=1):
+        """
+        See :meth:`pints.Boundaries.sample()`.
+
+        Note: This method is implemented only when the error measure is based
+        on a :class:`pints.LogPrior` that supports sampling.
+        """
+        if not self._pdf_is_prior:
+            raise NotImplementedError
+        return self._log_pdf.sample(n)
 
