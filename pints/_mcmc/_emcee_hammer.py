21f29ea43d3b518093a32c2b2d32c26fdda4c1a0
ben18785 <ben.c.lambert@gmail.com>
2018-09-09 15:37:25 +0100

    Implemented single chain updating version of emcee hammer

--- pints/_mcmc/_emcee_hammer.py
+++ pints/_mcmc/_emcee_hammer.py
@@ -1,5 +1,5 @@
 #
-# Differential evolution MCMC
+# Emcee hammer MCMC
 #
 # This file is part of PINTS.
 #  Copyright (c) 2017-2018, University of Oxford.
@@ -12,19 +12,15 @@ import pints
 import numpy as np
 
 
-class EmceeHammerMCMC(pints.MultiChainMCMC):
+class DifferentialEvolutionMCMC(pints.MultiChainMCMC):
     """
-    *Extends:* :class:`MultiChainMCMC`
-
     Uses the differential evolution algorithm described in 
-    Algorithm 3 in [1].
-
-    Initially, the group of chains are split into two random
-    subgroups, of equal sizes. Then we update chain k
-    in each group according to:
-
+    Algorithm 2 in [1].
+    
+    For k in 1:N:
     - Draw a walker X_j at random from the complementary
-        ensemble (i.e. the other group) without replacement.
+        ensemble (i.e. the group of chains not including k)
+        without replacement.
         
     - Sample z ~ g(z), (see below).
 
@@ -37,7 +33,7 @@ class EmceeHammerMCMC(pints.MultiChainMCMC):
     - if r <= q: X_k(t+1) = Y,
       else: X_k(t+1) = X_k(t).
 
-    where g(z) is proportional to 1 / sqrt(z) if z is in [1/a, a],
+    where g(z) is proportional to 1 / sqrt(z) if z is in [1 / a, a],
     or 0, otherwise, where a is a parameter set a priori (default is 2);
     N = number of chains (walkers).
 
@@ -47,15 +43,12 @@ class EmceeHammerMCMC(pints.MultiChainMCMC):
     """
 
     def __init__(self, chains, x0, sigma0=None):
-        super(EmceeHammerMCMC, self).__init__(chains, x0, sigma0)
+        super(DifferentialEvolutionMCMC, self).__init__(chains, x0, sigma0)
 
         # Need at least 3 chains
-        if self._chains < 4:
-            raise ValueError('Need at least 4 chains.')
-        if self._chains % 2 != 0:
-            raise ValueError('The number of chains must be an even integer.')
-        self._chains_half = int(float(self._chains) / 2.0)
-        
+        if self._chains < 10:
+            raise ValueError('Need at least 3 chains.')
+
         # Set initial state
         self._running = False
 
@@ -63,85 +56,34 @@ class EmceeHammerMCMC(pints.MultiChainMCMC):
         self._current = None
         self._current_logpdf = None
         self._proposed = None
-
-        #
-        # Default settings
-        #
-
-        # Gamma
-        self._gamma = 2.38 / np.sqrt(2 * self._dimension)
-
-        # Normal proposal std.
-        self._b = 0.01
         
-    def _split_chains(self):
-        """
-        Splits chains into two equal sized groups.
-        """
-        self._group_0 = np.arange(0, int(float(self._chains) / 2.0))
-        self._group_1 = np.arange(int(float(self._chains) / 2.0), self._chains)
+        # Hyper parameter
+        self._a = 2
 
     def ask(self):
-        """ See :meth:`pints.MultiChainMCMC.ask()`.
-            Updates the positin of the chains (walkers)
-            within the group being considered as per Algorithm 3 in [1]. The
-            returned group of positions is theferefore of length
-            equal to half the number of total chains
-            
-            [1] "emcee: The MCMC Hammer", Daniel Foreman-Mackey, David W. Hogg,
-            Dustin Lang, Jonathan Goodman, 2013, arXiv,
-            https://arxiv.org/pdf/1202.3665.pdf
-        """
+        """ See :meth:`pints.MultiChainMCMC.ask()`. """
         # Initialise on first call
         if not self._running:
             self._initialise()
         
-        if len(self._remaining) == self._chains_half and self._current_group == 0:
-            self._count = 0
-            self._proposed_all = np.zeros(self._current.shape)
-            self._current_copy = np.copy(self._current)
+        # Propose new points
+        if len(self._remaining) == 0:
+            self._remaining = np.arange(self._chains)
+        self._k = self._remaining[0]
+        self._remaining = np.delete(self._remaining, 0)
         
-        # Only runs after first tell
-        if self._proposed is None:
-            # If all updates for given group are done
-            # move onto the other
-            self._proposed = np.zeros(self._chains_half, self._dimension)
-            self._k = np.zeros(self._chains_half, dtype='int')
-            self._z = np.zeros(self._chains_half)
-            
-            if len(self._remaining) == 0:
-                # Select other
-                self._current_group = (self._current_group + 1) % 2
-                if self._current_group == 0:
-                    self._other_remaining = self._group_1
-                    self._current_members= self._group_0
-                else:
-                    self._other_remaining = self._group_0
-                    self._current_members = self._group_1
-
-            # Make proposals for all walkers within a given group,
-            # and save z for each of those
-            for i in range(0, self._chains_half):
-                # Randomly select a chain from the other group
-                self._other_remaining = np.random.permutation(self._other_remaining)
-                j = self._other_remaining[len(self._other_remaining) - 1]
-                self._other_remaining = np.delete(self._other_remaining,
-                                                  len(self._other_remaining) - 1)
-                k = self._current_members[i]
-                X_j = self._current_copy[j]
-                X_k = self._current_copy[k]
-                self._indices[self._count] = k
-
-                # Sample from g(z) = 1/sqrt(z) if z in [1/a,a], 0 otherwise
-                # using inverse transform sampling:
-                # inv-CDF(r,a) = (1 + (a - 1)r)^2/a
-                r = np.random.rand()
-                self._z[i] = ((1 + (self._a - 1) * r)**2) / self._a
-
-                # Create proposal
-                self._proposed[i] = X_j + self._z[i] * (X_k - X_j)
-                self._proposed_all[self._count] = self._proposed_all[i]
-                self._count += 1
+        # pick j from the complementary ensemble
+        j = np.random.randint(self._chains)
+        while j == self._k:
+            j = np.random.randint(self._chains)
+        X_j = self._current[j]
+        X_k = self._current[self._k]
+        
+        # sample Z from g[z] = (1/sqrt(Z)), if Z in [1/a, a],
+        # 0 otherwise
+        r = np.random.rand()
+        self._Z = ((1 + (self._a - 1) * r)**2) / self._a
+        self._proposed = X_j + self._Z * (X_k - X_j)
 
         # Set as read only
         self._proposed.setflags(write=False)
@@ -160,46 +102,25 @@ class EmceeHammerMCMC(pints.MultiChainMCMC):
         self._current = None
         self._current_log_pdfs = None
         self._proposed = self._x0
-
-        # Set mu
-        #TODO: Should this be a user setting?
-        self._mu = np.mean(self._x0, axis=0)
+        
+        # a range
+        self._remaining = np.arange(self._chains)
 
         # Update sampler state
         self._running = True
-        
-        # Split chains into two groups
-        self._split_chains()
-
-        # Set current group being updated
-        self._current_group = 0
-
-        # Set current remaining chains as group 1 (i.e.
-        # the second) as a starting complementary ensemble
-        self._other_remaining = self._group_1
-        self._current_members = self._group_0
-        self._count = 0
-        self._indices = np.zeros(self._chains)
-        
-        # Set initial z
-        self._z = None
 
     def name(self):
         """ See :meth:`pints.MCMCSampler.name()`. """
-        return 'Differential Evolution MCMC'
+        return 'Emcee Hammer MCMC'
 
     def tell(self, fx):
-        """ See :meth:`pints.MultiChainMCMC.tell()`. 
-        Accepts or rejects proposals for half the chains.
-        If first half is updated then return None; otherwise
-        return all the chains.
-        """
+        """ See :meth:`pints.MultiChainMCMC.tell()`. """
         # Check if we had a proposal
         if self._proposed is None:
             raise RuntimeError('Tell called before proposal was set.')
 
         # Ensure proposed_log_pdfs are numpy array
-        proposed_log_pdfs = np.array(proposed_log_pdfs)
+        proposed_log_pdfs = np.array(fx
 
         # First points?
         if self._current is None:
@@ -209,7 +130,7 @@ class EmceeHammerMCMC(pints.MultiChainMCMC):
 
             # Accept
             self._current = self._proposed
-            self._current_log_pdfs = fx
+            self._current_log_pdfs = proposed_log_pdfs
 
             # Clear proposal
             self._proposed = None
@@ -217,57 +138,38 @@ class EmceeHammerMCMC(pints.MultiChainMCMC):
             # Return first samples for chains
             return self._current
 
-        # For the chains being updated
-        if self._current_group == 0:
-            counter = 0
-        else:
-            counter = self._chains_half
-        
-        for i in range(0, self._chains_half):
-            r = ((self._z[i]**(self._chains - 1))  * 
-                 fx[i] \ self._current_log_pdfs[i])
-            u = np.random.rand()
+        r_log = np.log(np.random.rand())
+        q = ((self._chains - 1) * np.log(self._Z)  + proposed_log_pdfs[self._k] -
+                self._current_log_pdfs[self._k])
 
-            if r <= q:
-                self._current[self._indices[i]] = self._proposed
+        if q >= r_log:
+            self._current[self._k] = self._proposed
 
-    def set_normal_scale_coefficient(self, b):
-        """
-        Sets the normal scale coefficient ``b`` used in updating the position
-        of each chain.
-        """
-        b = float(b)
-        if b < 0:
-            raise ValueError('Normal scale coefficient must be non-negative.')
-        self._b = b
+        # Clear proposal
+        self._proposed = None
 
-    def set_gamma(self, gamma):
+        # Return samples to add to chains
+        self._current.setflags(write=False)
+        return self._current
+
+    def set_a(self, a):
         """
-        Sets the coefficient ``gamma`` used in updating the position of each
+        Sets the coefficient ``a`` used in updating the position of each
         chain.
         """
-        gamma = float(gamma)
-        if gamma < 0:
-            raise ValueError('Gamma must be non-negative.')
-        self._gamma = gamma
+        a = float(a)
+        if a <= 0:
+            raise ValueError('The a parameter must be positive.')
+        self._a = a
 
     def n_hyper_parameters(self):
         """ See :meth:`TunableMethod.n_hyper_parameters()`. """
-        return 2
+        return 1
 
     def set_hyper_parameters(self, x):
         """
-        The hyper-parameter vector is ``[gamma, normal_scale_coefficient]``.
+        The hyper-parameter vector is ``[a]``.
 
         See :meth:`TunableMethod.set_hyper_parameters()`.
         """
-        self.set_gamma(x[0])
-        self.set_normal_scale_coefficient(x[1])
-
-
-def r_draw(i, num_chains):
-    # TODO: Needs a docstring!
-    r1, r2 = np.random.choice(num_chains, 2, replace=False)
-    while(r1 == i or r2 == i or r1 == r2):
-        r1, r2 = np.random.choice(num_chains, 2, replace=False)
-    return r1, r2
+        self.set_a(x[0])
