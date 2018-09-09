8db5789d520a77a883cf3b37167326abd83a14b9
ben18785 <ben.c.lambert@gmail.com>
2018-09-09 16:23:29 +0100

    Added Galilean MC files

--- pints/__init__.py
+++ pints/__init__.py
@@ -171,6 +171,7 @@ from ._mcmc._dream import DreamMCMC
 from ._nested import NestedSampler
 from ._nested._rejection import NestedRejectionSampler
 from ._nested._ellipsoid import NestedEllipsoidSampler
+from ._nested._galilean_mc import GalileanMC
 
 
 #
