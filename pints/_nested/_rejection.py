a26e9d053b4a5c07735ce3470f055783c90413a9
Ben Lambert <ben.c.lambert@gmail.com>
2018-09-10 19:56:39 +0100

    Removed unnecessary dependencies

--- pints/_nested/_rejection.py
+++ pints/_nested/_rejection.py
@@ -10,8 +10,6 @@
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 import pints
-import numpy as np
-from scipy.misc import logsumexp
 
 
 class NestedRejectionSampler(pints.NestedSampler):
