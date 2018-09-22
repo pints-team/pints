2b075b3745d0a44a79f17d06f89e639de1506da6
Ben Lambert <ben.c.lambert@gmail.com>
2018-09-22 18:07:50 +0100

    Changed name for adaptive Metropolis

--- pints/_mcmc/_adaptive_metropolis_acmc.py
+++ pints/_mcmc/_adaptive_metropolis_acmc.py
@@ -12,20 +12,22 @@ import pints
 import numpy as np
 
 
-class AdaptiveMetropolisACMC(pints.AdaptiveCovarianceMCMC):
+class AdaptiveMetropolisACMCMC(pints.AdaptiveCovarianceMCMC):
     """
-    Adaptive Metropolis MCMC, as described by Algorithm 2 in [1],
-    (with gamma = self._adaptations ** -eta which isn't specified
-    in the paper)
+    Adaptive Metropolis adaptive covariance MCMC, as described by
+    Algorithm 2 in [1], (with gamma = self._adaptations ** -eta
+    which isn't specified in the paper).
 
     Initialises mu0 and sigma0 used in proposal N(mu0, lambda * sigma0)
     For iteration t = 0:n_iter:
       - Sample Y_t+1 ~ N(theta_t, lambda * sigma0_t)
-      - Calculate alpha(theta_t, Y_t+1) = min(1, p(Y_t+1|data) / p(theta_t|data))
+      - Calculate alpha(theta_t, Y_t+1) =
+                                        min(1, p(Y_t+1|data) / p(theta_t|data))
       - Set theta_t+1 = Y_t+1 with probability alpha(theta_t, Y_t+1); otherwise
       theta_t+1 = theta_t
       - Update mu_t+1 = mu_t + gamma_t+1 * (theta_t+1 - mu_t)
-      - Update sigma_t+1 = sigma_t + gamma_t+1 * ((theta_t+1 - mu_t)(theta_t+1 - mu_t)' - sigma_t)
+      - Update sigma_t+1 = sigma_t +
+                gamma_t+1 * ((theta_t+1 - mu_t)(theta_t+1 - mu_t)' - sigma_t)
     endfor
 
     [1] A tutorial on adaptive MCMC
@@ -35,11 +37,11 @@ class AdaptiveMetropolisACMC(pints.AdaptiveCovarianceMCMC):
     *Extends:* :class:`AdaptiveCovarianceMCMC`
     """
     def __init__(self, x0, sigma0=None):
-        super(AdaptiveMetropolisACMC, self).__init__(x0, sigma0)
+        super(AdaptiveMetropolisACMCMC, self).__init__(x0, sigma0)
 
     def ask(self):
         """ See :meth:`SingleChainMCMC.ask()`. """
-        super(AdaptiveMetropolisACMC, self).ask()
+        super(AdaptiveMetropolisACMCMC, self).ask()
 
         # Propose new point
         if self._proposed is None:
@@ -56,14 +58,13 @@ class AdaptiveMetropolisACMC(pints.AdaptiveCovarianceMCMC):
         """
         See :meth: `AdaptiveCovarianceMCMC._initialise()`.
         """
-        super(AdaptiveMetropolisACMC, self)._initialise()
+        super(AdaptiveMetropolisACMCMC, self)._initialise()
 
         # log adaptation
         self._lambda = (2.38**2) / self._dimension
 
     def tell(self, fx):
         """ See :meth:`pints.AdaptiveCovarianceMCMC.tell()`. """
-        super(AdaptiveMetropolisACMC, self).tell(fx)
+        super(AdaptiveMetropolisACMCMC, self).tell(fx)
         # Return new point for chain
         return self._current
-
