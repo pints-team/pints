a7bec409a99bcfa298252adaa9a35dcc195dc690
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-11-21 09:49:04 +0000

    Added code to split LogPDFs into LogPrior and LogLikelihood when logging Bayesian MCMC.

--- pints/tests/shared.py
+++ pints/tests/shared.py
@@ -6,6 +6,8 @@
 #  For licensing information, see the LICENSE file distributed with the PINTS
 #  software package.
 #
+from __future__ import absolute_import, division
+from __future__ import print_function, unicode_literals
 import os
 import sys
 import shutil
