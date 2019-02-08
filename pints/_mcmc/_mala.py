MALA now works! Wrong sign on log gradient (check that this is correct for log_pdfs)

pints/_mcmc/_mala.py
@@ -124,7 +124,7 @@ class MALAMCMC(pints.SingleChainMCMC):
         step_size = float(step_size)
         if step_size <= 0:
             raise ValueError('Step size must exceed 0.')
-        self._step_size = step_size
+        self._step_size = step_size * np.diag(self._sigma0)
 
     def step_size(self):
         """
@@ -147,15 +147,14 @@ class MALAMCMC(pints.SingleChainMCMC):
         # Propose new point
         if self._proposed is None:
             self._forward_mu = self._current + (self._step_size**2 / 2.0) * (
-                np.matmul(self._sigma0, self._current_gradient)
-            )
+                self._current_gradient)
             self._proposed = np.random.multivariate_normal(
-                self._forward_mu, self._step_size**2 * self._sigma0)
+                self._forward_mu,
+                self._step_size**2 * np.diag(np.ones(self._n_parameters)))
 
             self._forward_q = scipy.stats.multivariate_normal.logpdf(
-                self._proposed, self._forward_mu, self._step_size**2 * (
-                    self._sigma0
-                )
+                self._proposed, self._forward_mu,
+                self._step_size**2 * np.diag(np.ones(self._n_parameters))
             )
 
             # Set as read-only
@@ -182,7 +181,7 @@ class MALAMCMC(pints.SingleChainMCMC):
             # Accept
             self._current = self._proposed
             self._current_log_pdf = fx
-            self._current_gradient = -log_gradient
+            self._current_gradient = log_gradient
 
             # Increase iteration count
             self._iterations += 1
@@ -195,22 +194,18 @@ class MALAMCMC(pints.SingleChainMCMC):
 
         # Calculate alpha
         self._proposed_gradient = -log_gradient
-        self._backward_mu = self._proposed + (self._step_size**2 / 2.0) * (
-            np.matmul(self._sigma0, self._proposed_gradient)
+        self._backward_mu = self._proposed + (
+            (self._step_size**2 / 2.0) * self._proposed_gradient
         )
-        # print(self._proposed)
-        # print(self._sigma0)
-        # print(self._proposed_gradient)
-        # print(self._current)
-        # print(self._current_gradient)
-        # print(-1)
         self._backward_q = scipy.stats.multivariate_normal.logpdf(
             self._current, self._backward_mu, self._step_size**2 * (
-                self._sigma0
+                np.diag(np.ones(self._n_parameters))
             )
         )
         alpha = fx + self._backward_q - (
             self._current_log_pdf + self._forward_q)
+        # print(self._current)
+        # print(self._proposed)
         # print(alpha)
         # print(fx)
         # print(self._backward_q)