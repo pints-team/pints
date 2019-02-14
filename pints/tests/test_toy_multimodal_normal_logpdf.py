b3e1b0da76b7b65a593df78b51e4bac79470f139
Ben Lambert <ben.c.lambert@gmail.com>
2019-02-14 16:26:15 +0000

    Various additions to multimodal Gaussian
    - Added sensitivities
    - Added suggested bounds
    - Added KL divergence calculation by mode
    - Added distance measure (which sums KL divergence)
    - Made example notebook more comprehensive in terms of examples it shows
    - Added tests for everything (which pass on local machine)

--- pints/tests/test_toy_multimodal_normal_logpdf.py
+++ pints/tests/test_toy_multimodal_normal_logpdf.py
@@ -78,6 +78,95 @@ class TestMultimodalNormalLogPDF(unittest.TestCase):
             ValueError, pints.toy.MultimodalNormalLogPDF, None,
             [[[1, 0], [0]], [[1, 0], [0, 1]]])
 
+        # Test sensitivities
+        # default
+        log_pdf = pints.toy.MultimodalNormalLogPDF()
+        l, dl = log_pdf.evaluateS1([1, 1])
+        self.assertEqual(len(dl), 2)
+        self.assertAlmostEqual(l, -2.8378770664093453)
+        self.assertEqual(dl[0], -1)
+        self.assertEqual(dl[1], -1)
+
+        # non-standard
+        covariances = [[[5**2, 0.5 * 5 * 3, -0.1 * 5 * 2],
+                        [0.5 * 5 * 3, 3**2, 0.4 * 3 * 2],
+                        [-0.1 * 5 * 2, 0.4 * 3 * 2, 2**2]],
+                       [[5**2, 0.3 * 5 * 6, 0.2 * 5 * 7],
+                        [0.3 * 5 * 6, 6**2, -0.3 * 6 * 7],
+                        [0.2 * 5 * 7, -0.3 * 6 * 7, 7**2]]]
+        log_pdf = pints.toy.MultimodalNormalLogPDF(
+            modes=[[1, 2, 3], [3, 4, 5]],
+            covariances=covariances)
+        l, dl = log_pdf.evaluateS1([2, 4, 5])
+        self.assertEqual(len(dl), 3)
+        self.assertAlmostEqual(l, -6.2170334785680836)
+        self.assertAlmostEqual(dl[0], -0.024754331962513677)
+        self.assertAlmostEqual(dl[1], -0.054933536830093638)
+        self.assertAlmostEqual(dl[2], -0.39317158556789844)
+
+        # Test bounds
+        f = pints.toy.MultimodalNormalLogPDF()
+        bounds = f.suggested_bounds()
+        lower = -10
+        upper = 20
+        bounds1 = np.tile([lower, upper], (2, 1))
+        bounds1 = np.transpose(bounds1).tolist()
+        self.assertTrue(np.array_equal(bounds, bounds1))
+
+        f = pints.toy.MultimodalNormalLogPDF(
+            modes=[[1, 2, 3], [3, 4, 5]],
+            covariances=covariances)
+        bounds = f.suggested_bounds()
+        lower = 1 - 4
+        upper = 5 + 4
+        bounds1 = np.tile([lower, upper], (3, 1))
+        bounds1 = np.transpose(bounds1).tolist()
+        self.assertTrue(np.array_equal(bounds, bounds1))
+
+        # Test KL divergence
+        f = pints.toy.MultimodalNormalLogPDF()
+        n = 1000
+        d = f.n_parameters()
+        samples1 = f.sample(n)
+        self.assertEqual(samples1.shape, (n, d))
+        kl = f.kl_divergence(samples1)
+        for i in range(2):
+            self.assertTrue(kl[i] > 0)
+        self.assertEqual(np.sum(kl), f.distance(samples1))
+        x = np.ones((n, d + 1))
+        self.assertRaises(ValueError, f.kl_divergence, x)
+        x = np.ones((n, d, 2))
+        self.assertRaises(ValueError, f.kl_divergence, x)
+
+        f = pints.toy.MultimodalNormalLogPDF(
+            modes=[[1, 2, 3], [3, 4, 5]],
+            covariances=covariances)
+        n = 1000
+        d = f.n_parameters()
+        samples1 = f.sample(n)
+        self.assertEqual(samples1.shape, (n, d))
+        kl = f.kl_divergence(samples1)
+        for i in range(2):
+            self.assertTrue(kl[i] > 0)
+        self.assertEqual(np.sum(kl), f.distance(samples1))
+        x = np.ones((n, d + 1))
+        self.assertRaises(ValueError, f.kl_divergence, x)
+        x = np.ones((n, d, 2))
+        self.assertRaises(ValueError, f.kl_divergence, x)
+
+        f = pints.toy.MultimodalNormalLogPDF(modes=[[1], [2], [3]])
+        n = 1000
+        d = f.n_parameters()
+        samples1 = f.sample(n)
+        self.assertEqual(samples1.shape, (n, d))
+        kl = f.kl_divergence(samples1)
+        self.assertTrue(kl[0] > 0)
+        self.assertEqual(np.sum(kl), f.distance(samples1))
+        x = np.ones((n, d + 1))
+        self.assertRaises(ValueError, f.kl_divergence, x)
+        x = np.ones((n, d, 2))
+        self.assertRaises(ValueError, f.kl_divergence, x)
+
 
 if __name__ == '__main__':
     print('Add -v for more debug output')
