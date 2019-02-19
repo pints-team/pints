Corrected errorS with Gaussian log-likelihood!
- The sensitivities were previously not correct for multi-output problems
- Added multi-output value-based tests for multi-output problems
- Corrected mistake with constant_model forward sensitivities (it should be a diagonal matrix rather than one with increasing diagonals)
- Corrected errors with sensitivities for Gaussian log-likelihood for multiple output problems: formerly we were returning an array of theta derivatives followed by a single sigma derivative. This is incorrect as there are three sigma parameters - one for each output

pints/toy/_constant_model.py
@@ -106,8 +106,6 @@ class ConstantModel(pints.ForwardModelS1):
             #   [df2/dp1, df2/dp2]]
             # i.e.
             #  [[1, 0],
-            #   [0, 2]]
-            # i.e. np.diag(self._r)
-            dy = np.tile(np.diag(self._r), (len(times), 1, 1))
+            #   [0, 1]]
+            dy = np.tile(np.diag(np.ones(len(self._r))), (len(times), 1, 1))
         return (y, dy)
-