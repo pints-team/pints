Corrected errorS with Gaussian log-likelihood!
- The sensitivities were previously not correct for multi-output problems
- Added multi-output value-based tests for multi-output problems
- Corrected mistake with constant_model forward sensitivities (it should be a diagonal matrix rather than one with increasing diagonals)
- Corrected errors with sensitivities for Gaussian log-likelihood for multiple output problems: formerly we were returning an array of theta derivatives followed by a single sigma derivative. This is incorrect as there are three sigma parameters - one for each output

pints/_log_likelihoods.py
@@ -336,29 +336,27 @@ class GaussianLogLikelihood(pints.ProblemLogLikelihood):
 
     def evaluateS1(self, x):
         """ See :meth:`LogPDF.evaluateS1()`. """
-        sigma = float(np.asarray(x[-self._no:]))
+        sigma = np.asarray(x[-self._no:])
 
         # Evaluate, and get residuals
         y, dy = self._problem.evaluateS1(x[:-self._no])
 
         # Reshape dy, in case we're working with a single-output problem
-        dy = dy.reshape(self._nt, self._no, self._n_parameters - 1)
+        dy = dy.reshape(self._nt, self._no, self._n_parameters - self._no)
 
         # Note: Must be (data - simulation), sign now matters!
         r = self._values - y
 
         # Calculate log-likelihood
-        L = np.sum(-self._logn - self._nt * np.log(sigma)
-                   - (1.0 / (2 * sigma**2)) * np.sum(r**2, axis=0))
+        L = self.__call__(x)
 
         # Calculate derivatives in the model parameters
         dL = np.sum(
             (sigma**(-2.0) * np.sum((r.T * dy.T).T, axis=0).T).T, axis=0)
 
         # Calculate derivative wrt sigma
-        dsigma = np.sum(-self._nt / sigma +
-                        sigma**(-3.0) * np.sum(r**2, axis=0))
-        dL = np.concatenate((dL, np.array([dsigma])))
+        dsigma = -self._nt / sigma + sigma**(-3.0) * np.sum(r**2, axis=0)
+        dL = np.concatenate((dL, np.array(list(dsigma))))
 
         # Return
         return L, dL

pints/tests/test_log_likelihoods.py
@@ -136,6 +136,50 @@ class TestLogLikelihood(unittest.TestCase):
         l2 = pints.UnknownNoiseLogLikelihood(problem)
         self.assertIsInstance(l2, pints.GaussianLogLikelihood)
 
+        # test multiple output unknown noise
+        model = pints.toy.ConstantModel(3)
+        parameters = [0, 0, 0]
+        times = [1, 2, 3, 4]
+        values = model.simulate([0, 0, 0], times)
+        org_values = [[10.7, 3.5, 3.8],
+                      [1.1, 3.2, -1.4],
+                      [9.3, 0.0, 4.5],
+                      [1.2, -3, -10]]
+        problem = pints.MultiOutputProblem(model, times, org_values)
+        log_likelihood = pints.GaussianLogLikelihood(problem)
+        # Test Gaussian_logpdf((10.7, 1.1, 9.3, 1.2)|mean=0, sigma=3.5) +
+        #      Gaussian_logpdf((3.5, 3.2, 0.0, -3)|mean=0, sigma=1) +
+        #      Gaussian_logpdf((3.8, -1.4, 4.5, -10)|mean=0, sigma=12)
+        #      = -50.5088...
+        self.assertAlmostEqual(
+            log_likelihood(parameters + [3.5, 1, 12]),
+            -50.508848609684783
+        )
+        l, dl = log_likelihood.evaluateS1(parameters + [3.5, 1, 12])
+        self.assertAlmostEqual(l, -50.508848609684783)
+        self.assertAlmostEqual(dl[0], 1.820408163265306)
+        self.assertAlmostEqual(dl[1], 3.7000000000000002)
+        self.assertAlmostEqual(dl[2], -0.021527777777777774)
+        self.assertAlmostEqual(dl[3], 3.6065306122448981)
+        self.assertAlmostEqual(dl[4], 27.490000000000002)
+        self.assertAlmostEqual(dl[5], -0.25425347222222222)
+
+        # test multiple output model dimensions of sensitivities
+        d = 20
+        model = pints.toy.ConstantModel(d)
+        parameters = [0 for i in range(d)]
+        times = [1, 2, 3, 4]
+        values = model.simulate(parameters, times)
+        org_values = np.ones((len(times), d))
+        extra_params = np.ones(d).tolist()
+        problem = pints.MultiOutputProblem(model, times, org_values)
+        log_likelihood = pints.GaussianLogLikelihood(problem)
+        l = log_likelihood(parameters + extra_params)
+        l1, dl = log_likelihood.evaluateS1(parameters + extra_params)
+        self.assertTrue(np.array_equal(len(dl),
+                                       len(parameters + extra_params)))
+        self.assertEqual(l, l1)
+
     def test_known_noise_gaussian_single_S1(self):
         """
         Simple tests for single known noise Gaussian log-likelihood with
@@ -507,7 +551,7 @@ class TestLogLikelihood(unittest.TestCase):
                              [-10, -30.5, -5, 7.6],
                              [-12, -10.1, -4, 2.3]])
         problem = pints.MultiOutputProblem(model, times, values)
-        log_likelihood = pints.AR1LogLikelihood(problem)
+        log_likelihood = pints.ARMA11LogLikelihood(problem)
         # ARMA1Logpdf((3.5,1.1,-10, -12)|mean=0, rho=0.5, phi=0.34 sigma=1) +
         # ARMA1Logpdf((7.6,-10.3,-30.5, -10.1)|
         #             mean=0, rho=-0.25, phi=0.1, sigma=3) +