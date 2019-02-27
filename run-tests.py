5ecf0cc852f228023d7ecf9c8f3df9849fdce81b
Fergus Cooper <fcooper8472@gmail.com>
2019-02-27 16:44:33 +0000

    #668 Test that all classes and functions are documented in rst files
    Bonus checks for duplicate documentation and modules like numpy leaking through pints public interfaces

--- run-tests.py
+++ run-tests.py
@@ -87,6 +87,10 @@ def run_doctests():
     # Check all example notebooks are in the index
     doctest_examples_readme()
 
+    # Check all classes and methods are documented in rst files, and no
+    # unintended modules are exposed via a public interface
+    doctest_rst_and_public_interface()
+
 
 def doctest_sphinx():
     """
@@ -146,6 +150,163 @@ def doctest_examples_readme():
         sys.exit(1)
     else:
         print('All example notebooks are indexed.')
+
+
+def doctest_rst_and_public_interface():
+    """
+    Check that every class and method is documented in an rst file and that
+    no unintended modules are exposed via a public interface
+    """
+    print('\nChecking that all classes and methods are documented in an RST '
+          'file and that public interfaces are clean.')
+
+    # Import all public pints modules. Importing everything here is not
+    # strictly needed: `import pints` happens to import `pints.noise`, and
+    # `import pints.toy` will import `pints` and `pints.toy`, but we list
+    # everything here for completeness. If a new module is added to pints
+    # it should be imported here for this doctest.
+    import pints
+    import pints.io
+    import pints.noise
+    import pints.plot
+    import pints.toy
+
+    # If any modules other than these are exposed it may indicate that a module
+    # has been inadvertently exposed in a public context, or that a new module
+    # has been added to pints and should be imported above and included in this
+    # list.
+    pints_submodules = ['pints.io', 'pints.noise', 'pints.plot', 'pints.toy']
+
+    doc_symbols = get_all_documented_symbols()
+
+    check_exposed_symbols(pints, pints_submodules, doc_symbols)
+    check_exposed_symbols(pints.io, [], doc_symbols)
+    check_exposed_symbols(pints.noise, [], doc_symbols)
+    check_exposed_symbols(pints.plot, [], doc_symbols)
+    check_exposed_symbols(pints.toy, [], doc_symbols)
+
+    print('All classes and methods are documented in an RST file, and all '
+          'public interfaces are clean.')
+
+
+def check_exposed_symbols(_module, _submodule_names, _doc_symbols):
+    """
+    Check _module for any classes and methods not contained in _doc_symbols,
+    and check for any modules not contained in _submodule_names.
+
+    :param _module: the module to check
+    :param _submodule_names: list of submodules exposed by _module
+    :param _doc_symbols: dictionary containing lists of documented classes and
+     functions
+    """
+
+    import inspect
+    exposed_symbols = [x for x in dir(_module) if not x.startswith('_')]
+    symbols = [getattr(_module, exposed_symbols[x]) for x in
+               range(len(exposed_symbols))]
+
+    classes = [x for x in symbols if inspect.isclass(x)]
+    functions = [x for x in symbols if inspect.isfunction(x)]
+
+    # Check for modules: these should match perfectly with _submodule_names
+    exposed_modules = [x for x in symbols if inspect.ismodule(x)]
+    unexpected_modules = [m for m in exposed_modules if
+                          m.__name__ not in _submodule_names]
+
+    if len(unexpected_modules) > 0:
+        print('The following modules are unexpectedly exposed in the public '
+              'interface of %s:' % _module.__name__)
+        for m in sorted(unexpected_modules):
+            print('  unexpected module: ' + m.__name__)
+
+        print('For python modules such as numpy you may need to confine the '
+              'import to the function scope. If you have created a new pints '
+              'submodule, you will need to make %s (doctest) aware of this.'
+              % __file__)
+        print('FAILED')
+        sys.exit(1)
+
+    # Check that all classes are documented
+    undocumented_classes = []
+    for _class in classes:
+        class_name = _module.__name__ + '.' + _class.__name__
+        if class_name not in _doc_symbols['classes']:
+            undocumented_classes.append(class_name)
+
+    if len(undocumented_classes) > 0:
+        print('The following classes do not appear in any RST file:')
+        for m in sorted(undocumented_classes):
+            print('  undocumented class: ' + m)
+        print('FAILED')
+        sys.exit(1)
+
+    # Check that all functions are documented
+    undocumented_functions = []
+    for _funct in functions:
+        funct_name = _module.__name__ + '.' + _funct.__name__
+        if funct_name not in _doc_symbols['functions']:
+            undocumented_functions.append(funct_name)
+
+    if len(undocumented_functions) > 0:
+        print('The following functions do not appear in any RST file:')
+        for m in sorted(undocumented_functions):
+            print('  undocumented function: ' + m)
+        print('FAILED')
+        sys.exit(1)
+
+
+def get_all_documented_symbols():
+    """
+    Recursively traverse docs/source and identify all autoclass and
+    autofunction declarations.
+
+    :return: A dict containing a list of classes and a list of functions
+    """
+
+    doc_files = []
+    for root, dirs, files in os.walk(os.path.join('docs', 'source')):
+        for file in files:
+            if file.endswith('.rst'):
+                doc_files.append(os.path.join(root, file))
+
+    # Regular expressions to find autoclass and autofunction specifiers
+    regex_module = re.compile(r'\.\.\s*\S*module\:\:\s*(\S+)')
+    regex_class = re.compile(r'\.\.\s*autoclass\:\:\s*(\S+)')
+    regex_funct = re.compile(r'\.\.\s*autofunction\:\:\s*(\S+)')
+
+    # Identify all instances of autoclass and autofunction in all rst files
+    documented_symbols = {'classes': [], 'functions': []}
+    for doc_file in doc_files:
+        with open(doc_file, 'r') as f:
+            # We need to identify which module each class or function is in
+            module = ''
+            for line in f.readlines():
+                m_match = re.search(regex_module, line)
+                c_match = re.search(regex_class, line)
+                f_match = re.search(regex_funct, line)
+                if m_match:
+                    module = m_match.group(1) + '.'
+                elif c_match:
+                    documented_symbols['classes'].append(
+                        module + c_match.group(1))
+                elif f_match:
+                    documented_symbols['functions'].append(
+                        module + f_match.group(1))
+
+    # Validate the list for any duplicate documentation
+    for symbols in documented_symbols.values():
+        if len(set(symbols)) != len(symbols):
+            print('The following symbols are unexpectedly documented multiple '
+                  'times in rst files:')
+
+            dupes = set([d for d in symbols if symbols.count(d) > 1])
+            for d in dupes:
+                print('  multiple entries in docs: ' + d)
+
+            print('FAILED')
+            sys.exit(1)
+
+    return documented_symbols
 
 
 def run_notebook_tests(skip_slow_books=False, executable='python'):
