80b90c0bdde8188a24927fecf871a7f6d16db6a7
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-04-17 20:58:50 +0100

    Made setup.py use version file, and include it when installing the package.

--- setup.py
+++ setup.py
@@ -12,22 +12,45 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
+
+# Read version number from file
+def load_version():
+    try:
+        import os
+        root = os.path.abspath(os.path.dirname(__file__))
+        with open(os.path.join(root, 'pints', 'version'), 'r') as f:
+            version = f.read().strip().split(',')
+        return '.'.join([str(int(x)) for x in version])
+    except Exception as e:
+        raise RuntimeError('Unable to read version number (' + str(e) + ').')
+
+
 # Go!
 setup(
     # Module name (lowercase)
     name='pints',
-    # Remember to keep this in sync with pints/__init__.py
-    version='0.0.1',
+    version=load_version(),
+
+    # Description
     description='Probabilistic Inference in Noisy Time-Series',
     long_description=readme,
+
+    # License name
     license='BSD 3-clause license',
+
+    # Maintainer information
     # author='',
     # author_email='',
     maintainer='Michael Clerx',
     maintainer_email='michael.clerx@cs.ox.ac.uk',
     url='https://github.com/pints-team/pints',
+
     # Packages to include
     packages=find_packages(include=('pints', 'pints.*')),
+
+    # Include non-python files (via MANIFEST.in)
+    include_package_data=True,
+
     # List of dependencies
     install_requires=[
         'cma>=2',
