7c4752b10611aafd60473d51472c510ba331d45d
lorcandelaney <lorcandelaney.96@gmail.com>
2019-08-12 15:50:15 +0100

    Add documentation and correct tests.

--- pints/_mcmc/_slice_rank_shrinking.py
+++ pints/_mcmc/_slice_rank_shrinking.py
@@ -15,6 +15,58 @@ import numpy as np
 
 class SliceRankShrinkingMCMC(pints.SingleChainMCMC):
     """
+    Implements Covariance-Adaptive Slice Sampling by Rank Shrinking,
+    as described in [1]. This is an adaptive multivariate method which
+    uses additional points, called "crumbs", and rejected proposals to
+    guide the selection of samples.
+
+    It generates samples by sampling uniformly from the volume underneath the
+    posterior (``f``). It does so by introducing an auxiliary variable (``y``)
+    and by definying a Markov chain.
+
+    Sampling follows:
+
+    1. Calculate the pdf (``f(x_0)``) of the current sample (``x_0``).
+    2. Draw a real value (``y``) uniformly from (0, f(x0)), defining a
+       horizontal “slice”: S = {x: y < f(x)}. Note that ``x_0`` is
+       always within S.
+    3. Draw the first crumb (``c_1``) from a Gaussian distribution with
+       mean ``x_0`` and precision matrix ``W_1``.
+    4. Draw a new point (``x_1``) from a Gaussian distribution with mean
+       ``c_1`` and precision matrix ``W_2``.
+
+    New crumbs are drawn until a new proposal is accepted. In particular,
+    after sampling ``k`` crumbs from Gaussian distributions with mean ``x0``
+    and precision matrices (``W_1``, ..., ``W_k``), the distribution for the
+    ``k`` th proposal sample is:
+
+    1. ``x_k \sim Normal(\bar{c}_k, \Lambda^{-1}_k)``
+
+    where:
+
+    2. ``\Lambda_k = W_1 + ... + W_k``
+    3. ``\bar{c}_k = \Lambda^{-1}_k * (W_1 * c_1 + ... + W_k * c_k)``
+
+    This method aims to conveniently modify the ``k + 1`` th proposal
+    distribution to increase the likelihood of sampling an acceptable point.
+    It does so by calculating the gradient (``g(f(x))``) of the unnormalised
+    posterior (``f(x)``) at the last rejected point (``x_k``).
+    It then sets the conditional variance of the ``k + 1`` proposal
+    distribution in the direction of the gradient ``g(f(x_k))`` to 0.
+    This is reasonable in that the gradient at a proposal probably points in
+    a direction where the variance is small, so it is more efficient to move
+    in a different direction.
+
+    To avoid floating-point underflow, we implement the suggestion advanced
+    in [1] pp.712. We use the log pdf of the un-normalised posterior
+    (``log f(x)``) instead of ``f(x)``. In doing so, we use an
+    auxiliary variable ``z = log(y) - \epsilon``, where
+    ``\epsilon \sim \text{exp}(1)`` and define the slice as
+    S = {x : z < log f(x)}.
+
+    [1] Thompson, M. and Neal, R.M., 2010. Covariance-adaptive slice sampling.
+    arXiv preprint arXiv:1003.3201.
+
     *Extends:* :class:`SingleChainMCMC`
     """
 
@@ -91,6 +143,51 @@ class SliceRankShrinkingMCMC(pints.SingleChainMCMC):
         self._ready_for_tell = True
         return np.array(self._proposed, copy=True)
 
+    def current_log_pdf(self):
+        """ See :meth:`SingleChainMCMC.current_log_pdf()`. """
+        return np.copy(self._current_log_pdf)
+
+    def current_slice_height(self):
+        """
+        Returns the height of the current slice.
+        """
+        return self._current_log_y
+
+    def name(self):
+        """ See :meth:`pints.MCMCSampler.name()`. """
+        return 'Slice Sampling - Covariance-Adaptive: Rank Shrinking.'
+
+    def needs_sensitivities(self):
+        """ See :meth:`pints.MCMCSampler.needs_sensitivities()`. """
+        return True
+
+    def n_hyper_parameters(self):
+        """ See :meth:`TunableMethod.n_hyper_parameters()`. """
+        return 1
+
+    def set_hyper_parameters(self, x):
+        """
+        The hyper-parameter vector is ``[sigma_c]``.
+        See :meth:`TunableMethod.set_hyper_parameters()`.
+        """
+        self.set_sigma_c(x[0])
+
+    def set_sigma_c(self, sigma_c):
+        """
+        Sets standard deviation of initial crumb distribution.
+        """
+        sigma_c = float(sigma_c)
+        if sigma_c < 0:
+            raise ValueError('Inital crumb standard deviation '
+                             'must be positive.')
+        self._sigma_c = sigma_c
+
+    def sigma_c(self):
+        """
+        Returns standard deviation of initial crumb distribution.
+        """
+        return self._sigma_c
+
     def tell(self, reply):
         """ See :meth:`pints.SingleChainMCMC.tell()`. """
 
@@ -144,7 +241,10 @@ class SliceRankShrinkingMCMC(pints.SingleChainMCMC):
             # Return accepted sample
             return np.array(self._proposed, copy=True)
 
-        # If proposal is reject, shrink rank of matrix J
+        # If proposal is reject, shrink rank of the next proposal distribution
+        # by adding new orthonormal column to ``J``. This will represent a new
+        # direction in which the conditional covariance of the proposal
+        # distribution will be 0.
         else:
             # If J has less non-zero columns than``number of
             # parameters - 1``, shrink rank by adding new orthonormal
@@ -163,44 +263,3 @@ class SliceRankShrinkingMCMC(pints.SingleChainMCMC):
                     self._J = np.column_stack([self._J,
                                                new_column])
             return None
-
-    def name(self):
-        """ See :meth:`pints.MCMCSampler.name()`. """
-        return 'Slice Sampling, Covariance Adaptive: Rank Shrinking'
-
-    def needs_sensitivities(self):
-        """ See :meth:`pints.MCMCSampler.needs_sensitivities()`. """
-        return True
-
-    def set_sigma_c(self, sigma_c):
-        """
-        Sets standard deviation of initial crumb.
-        """
-        sigma_c = float(sigma_c)
-        if sigma_c < 0:
-            raise ValueError("""Inital crumb standard deviationn
-                             must be positive.""")
-        self._sigma_c = sigma_c
-
-    def get_sigma_c(self):
-        """
-        Returns standard deviation of initial crumb.
-        """
-        return self._sigma_c
-
-    def get_current_slice_height(self):
-        """
-        Returns current log_y used to define the current slice.
-        """
-        return self._current_log_y
-
-    def n_hyper_parameters(self):
-        """ See :meth:`TunableMethod.n_hyper_parameters()`. """
-        return 1
-
-    def set_hyper_parameters(self, x):
-        """
-        The hyper-parameter vector is ``[sigma_c]``.
-        See :meth:`TunableMethod.set_hyper_parameters()`.
-        """
-        self.set_sigma_c(x[0])
--- pints/tests/test_mcmc_slice_rank_shrinking.py
+++ pints/tests/test_mcmc_slice_rank_shrinking.py
@@ -13,148 +13,99 @@ import unittest
 import numpy as np
 
 import pints
-import pints.toy as toy
+import pints.toy
 
 debug = False
 
 
 class TestSliceRankShrinking(unittest.TestCase):
     """
-    Tests the basic methods of the Slice Sampling Rank Shrink
+    Tests the basic methods of the Slice Sampling Rank Shrinking
     routine.
     """
-    def test_initialisation(self):
-        """
-        Tests whether all instance attributes are initialised correctly.
-        """
-        # Create mcmc
-        x0 = np.array([2, 4])
-        mcmc = pints.SliceRankShrinkingMCMC(x0)
-
-        # Test attributes initialisation
-        self.assertFalse(mcmc._running)
-        self.assertFalse(mcmc._ready_for_tell)
-        self.assertEqual(mcmc._current, None)
-        self.assertEqual(mcmc._current_log_y, None)
-        self.assertEqual(mcmc._proposed, None)
-        self.assertEqual(mcmc._k, 0)
-        self.assertEqual(mcmc._c_bar, 0)
-
     def test_first_run(self):
-        """
-        #Tests the very first run of the sampler.
-        """
+        # Tests the very first run of the sampler.
+
         # Create log pdf
-        log_pdf = toy.GaussianLogPDF([2, 4], [[1, 0], [0, 3]])
+        log_pdf = pints.toy.GaussianLogPDF([2, 4], [[1, 0], [0, 3]])
 
         # Create mcmc
         x0 = np.array([2., 4.])
         mcmc = pints.SliceRankShrinkingMCMC(x0)
 
-        # Ask should fail if _ready_for_tell flag is True
+        # Ask() should fail if _ready_for_tell flag is True
+        x = mcmc.ask()
         with self.assertRaises(RuntimeError):
-            mcmc._ready_for_tell = True
             mcmc.ask()
 
-        # Undo changes
-        mcmc._ready_for_tell = False
-
-        # Check whether _running flag becomes True when ask() is called
         # Check whether first iteration of ask() returns x0
-        self.assertFalse(mcmc._running)
-        self.assertTrue(np.all(mcmc.ask() == x0))
-        self.assertTrue(mcmc._running)
-        self.assertTrue(mcmc._ready_for_tell)
+        self.assertTrue(np.all(x == x0))
 
         # Calculate log pdf for x0
         fx, grad = log_pdf.evaluateS1(x0)
 
-        # Tell should fail when _ready_for_tell is False
-        with self.assertRaises(RuntimeError):
-            mcmc._ready_for_tell = False
-            mcmc.tell((fx, grad))
-
-        # Undo changes
-        mcmc._ready_for_tell = True
-
         # Test first iteration of tell(). The first point in the chain
         # should be x0
-        sample_0 = mcmc.tell((fx, grad))
-        self.assertTrue(np.all(sample_0 == x0))
-
-        # We update the current sample
-        self.assertTrue(np.all(mcmc._current == x0))
-        self.assertTrue(np.all(mcmc._current == mcmc._proposed))
+        sample = mcmc.tell((fx, grad))
+        self.assertTrue(np.all(sample == x0))
 
         # We update the _current_log_pdf value used to generate the new slice
-        self.assertEqual(mcmc._current_log_pdf, fx)
+        self.assertEqual(mcmc.current_log_pdf(), fx)
 
         # Check that the new slice has been constructed appropriately
-        self.assertTrue(mcmc._current_log_y < mcmc._current_log_pdf)
+        self.assertTrue(
+            mcmc.current_slice_height() < mcmc.current_log_pdf())
 
-    def test_mcmc_step(self):
-        # Set seed
-        np.random.seed(2)
-
-        # Create log pdf
-        log_pdf = pints.toy.GaussianLogPDF([2, 4], [[1, 0], [0, 3]])
-
-        # Create mcmc
-        x0 = np.array([2., 4.])
+        # Tell() should fail when fx is infinite
         mcmc = pints.SliceRankShrinkingMCMC(x0)
+        mcmc.ask()
+        with self.assertRaises(ValueError):
+            fx = np.inf
+            mcmc.tell((fx, grad))
 
-        # Set initial crumb standard deviation
-        mcmc.set_sigma_c(1)
+        # Tell() should fail when _ready_for_tell is False
+        with self.assertRaises(RuntimeError):
+            mcmc.tell((fx, grad))
 
-        # First iteration
-        x = mcmc.ask()
-        fx, grad = log_pdf.evaluateS1(x)
-        sample_0 = mcmc.tell((fx, grad))
-        self.assertTrue(np.all(sample_0 == x0))
+        # Test sensitivities
+        self.assertTrue(mcmc.needs_sensitivities())
 
-        # Second iteration
-        x = mcmc.ask()
-        self.assertEqual(mcmc._k, 1)
-        fx, grad = log_pdf.evaluateS1(x)
-        sample1 = mcmc.tell((fx, grad))
-        self.assertTrue(sample1 is None)
-        self.assertTrue(mcmc._c_bar is not True)
-
-        # Third iteration
-        x = mcmc.ask()
-        self.assertEqual(mcmc._k, 2)
-        fx, grad = log_pdf.evaluateS1(x)
-        sample2 = mcmc.tell((fx, grad))
-        self.assertTrue(np.all(sample2 == mcmc._current))
-        self.assertEqual(mcmc._k, 0)
-        self.assertEqual(mcmc._c_bar, 0)
-
-        # Fourth iteration
-        x = mcmc.ask()
-        self.assertEqual(mcmc._k, 1)
-        self.assertTrue(np.all(sample2 == mcmc._current))
-        fx, grad = log_pdf.evaluateS1(x)
-        sample3 = mcmc.tell((fx, grad))
-        self.assertTrue(sample3 is None)
-
-        # Fourth iteration
-        x = mcmc.ask()
-        self.assertEqual(mcmc._k, 2)
-        fx, grad = log_pdf.evaluateS1(x)
-        sample3 = mcmc.tell((fx, grad))
-        self.assertEqual(mcmc._k, 0)
-        self.assertEqual(mcmc._c_bar, 0)
-
-    def test_run(self):
-        # Set seed for monitoring
-        np.random.seed(2)
-
-        # Create log pdf
-        log_pdf = pints.toy.GaussianLogPDF([2, 4], [[1, 0], [0, 3]])
+    def test_basic(self):
+        # Test basic methods of the class.
 
         # Create mcmc
         x0 = np.array([1, 1])
         mcmc = pints.SliceRankShrinkingMCMC(x0)
+
+        # Test name
+        self.assertEqual(
+            mcmc.name(),
+            'Slice Sampling - Covariance-Adaptive: Rank Shrinking.')
+
+        # Test set_sigma_c(), sigma_c()
+        mcmc.set_sigma_c(6)
+        self.assertEqual(mcmc.sigma_c(), 6)
+        with self.assertRaises(ValueError):
+            mcmc.set_sigma_c(-1)
+
+        # Test number of hyperparameters
+        self.assertEqual(mcmc.n_hyper_parameters(), 1)
+
+        # Test setting hyperparameters
+        mcmc.set_hyper_parameters([33])
+        self.assertEqual(mcmc.sigma_c(), 33)
+
+    def test_run(self):
+        # Tests complete run.
+
+        # Create log pdf
+        log_pdf = pints.toy.MultimodalGaussianLogPDF(
+            modes=[[0, 2], [0, 7], [5, 0], [4, 4]])
+
+        # Create non-adaptive mcmc
+        x0 = np.array([1, 1])
+        mcmc = pints.SliceRankShrinkingMCMC(x0)
+        mcmc.set_sigma_c(3)
 
         # Run multiple iterations of the sampler
         chain = []
@@ -164,35 +115,12 @@ class TestSliceRankShrinking(unittest.TestCase):
             sample = mcmc.tell((fx, grad))
             if sample is not None:
                 chain.append(np.copy(sample))
+        self.assertEqual(np.shape(chain), (100, 2))
 
-        # Fit Multivariate Gaussian to chain samples
-        #print(np.mean(chain, axis=0))
-        #print(np.cov(chain, rowvar=0))
 
-    def test_basic(self):
-        # Create mcmc
-        x0 = np.array([1, 1])
-        mcmc = pints.SliceRankShrinkingMCMC(x0)
-
-        # Test name
-        self.assertEqual(mcmc.name(),
-                         'Slice Sampling, Covariance Adaptive: Rank Shrinking')
-
-        # Test set_w
-        mcmc.set_sigma_c(3)
-        self.assertEqual(mcmc._sigma_c, 3)
-        with self.assertRaises(ValueError):
-            mcmc.set_sigma_c(-1)
-
-        # Test get_w
-        self.assertEqual(mcmc.get_sigma_c(), 3)
-
-        # Test current_slice height
-        self.assertEqual(mcmc.get_current_slice_height(), mcmc._current_log_y)
-
-        # Test number of hyperparameters
-        self.assertEqual(mcmc.n_hyper_parameters(), 1)
-
-        # Test setting hyperparameters
-        mcmc.set_hyper_parameters([10])
-        self.assertEqual(mcmc._sigma_c, 10)
+if __name__ == '__main__':
+    print('Add -v for more debug output')
+    import sys
+    if '-v' in sys.argv:
+        debug = True
+    unittest.main()
