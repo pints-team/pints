1b5c0747f82fe808ada1fdf808fc9bf3ce500d0d
ben18785 <ben.c.lambert@gmail.com>
2019-09-26 00:13:29 +0100

    tests for fitzhugh-nagumo model

--- pints/tests/test_toy_fitzhugh_nagumo_model.py
+++ pints/tests/test_toy_fitzhugh_nagumo_model.py
@@ -10,6 +10,7 @@
 import unittest
 import pints
 import pints.toy
+import numpy as np
 
 
 class TestFitzhughNagumoModel(unittest.TestCase):
@@ -47,6 +48,19 @@ class TestFitzhughNagumoModel(unittest.TestCase):
         # Initial value must have size 2
         pints.toy.FitzhughNagumoModel([1, 1])
         self.assertRaises(ValueError, pints.toy.FitzhughNagumoModel, [1])
+
+    def test_values(self):
+        # value-based tests of Fitzhugh-Nagumo model
+        parameters = [0.2, 0.4, 2.5]
+        y0 = [-2, 1.5]
+        times = np.linspace(0, 20, 201)
+        model = pints.toy.FitzhughNagumoModel(y0)
+        values = model.simulate(parameters, times)
+        self.assertTrue(np.abs(values[200, 0] - 1.6757257661845268) <
+                        0.000001)
+        self.assertTrue(np.abs(values[200, 1] - -0.22614224385943715) <
+                        0.000001)
+
 
 
 if __name__ == '__main__':
