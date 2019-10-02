b717fe0605f5b4c70a6fd90b338f0d5f97f71651
ben18785 <ben.c.lambert@gmail.com>
2019-10-02 12:12:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         # on systems without an attached display, it should never be imported
         # outside of plot() methods.
         'matplotlib>=1.5',
+        'tabulate>0.1',
     ],
     extras_require={
         'docs': [
@@ -73,4 +74,3 @@ setup(
         ],
     },
 )
-
