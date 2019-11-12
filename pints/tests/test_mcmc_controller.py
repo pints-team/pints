1c6192d2adf0ac08b59aeea7136f1906a6828603
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-11-12 16:17:31 +0000

    Better test for storing output from single chain mcmc with Nones at random indices. Uncovers bug

--- pints/tests/test_mcmc_controller.py
+++ pints/tests/test_mcmc_controller.py
@@ -1155,7 +1155,7 @@ class TestMCMCControllerSingleChainStorage(unittest.TestCase):
     def test_multiple_samplers_mixed_index_nones(self):
         """ Multiple single-chain samplers, None at different indices. """
         chains1 = [
-            [[2], None, [4], [6], None, [3], None, None, None, [5]],
+            [[2], None, [4], [6], None, [3], [5], None, None, None],
             [[5], None, None, [1], [3], None, [3], None, None, [2]],
         ]
         chains1, chains2, chains3, log_pdfs1, log_pdfs2 = self.go(chains1)
@@ -1167,7 +1167,7 @@ class TestMCMCControllerSingleChainStorage(unittest.TestCase):
         self.assertTrue(np.all(log_pdfs1 == log_pdfs2))
 
         chains1 = [
-            [[1, 2], [4, 3], [6, 1], None, None, None, [2, 2], None, [5, 7]],
+            [[1, 2], [4, 3], [6, 1], None, [5, 7], [2, 2], None, None],
             [[4, 3], None, [1, 1], [3, 5], None, None, [1, 4], [4, 7]],
         ]
         chains1, chains2, chains3, log_pdfs1, log_pdfs2 = self.go(chains1)
