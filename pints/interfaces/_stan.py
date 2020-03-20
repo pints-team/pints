9c20c6a0d98587ca8cb8148dac94498a12c88c5a
Ben Lambert <ben.c.lambert@gmail.com>
2020-03-20 16:45:28 +0000

    made interface compile Stan code and added pickled_form for ease of pickling

--- pints/interfaces/_stan.py
+++ pints/interfaces/_stan.py
@@ -9,15 +9,21 @@ from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 import numpy as np
 from collections import Counter
-import pints
+import pystan
 
 from . import InterfaceLogPDF
 
 
 class StanLogPDF(InterfaceLogPDF):
-    def __init__(self, stanfit, rectangular_boundaries=None):
-        if not stanfit.__class__.__name__ == 'StanFit4Model':
-            raise ValueError('Stan fit object must be of class StanFit4Model.')
+    def __init__(self, stan_code, stan_data):
+
+        print("Compiling Stan model then test running...")
+        sm = pystan.StanModel(model_code=stan_code)
+        stanfit = sm.sampling(data=stan_data, iter=1, chains=1,
+                              verbose=False, refresh=10,
+                              control={'adapt_engaged': False})
+        print("Stan model compiled and runs ok...ignore various warnings.")
+        self._compiled_stan = sm
         self._fit = stanfit
         self._log_prob = stanfit.log_prob
         self._grad_log_prob = stanfit.grad_log_prob
@@ -28,30 +34,14 @@ class StanLogPDF(InterfaceLogPDF):
         self._long_names = names
         self._counter = Counter(self._index)
         self._dict = {self._names[i]: [] for i in range(len(self._names))}
-        self._boundaries = None
-        self._lower = None
-        self._upper = None
-        if rectangular_boundaries is not None:
-            if not isinstance(rectangular_boundaries,
-                              pints.RectangularBoundaries):
-                raise ValueError('Any boundaries must be of class ' +
-                                 '`pints.RectangularBoundaries`.')
-            if rectangular_boundaries.n_parameters() != self._n_parameters:
-                raise ValueError('Dimensionality of boundaries must equal ' +
-                                 'that of Stan fit.')
-            self._boundaries = rectangular_boundaries
-            self._lower = rectangular_boundaries.lower()
-            self._upper = rectangular_boundaries.upper()
 
     def __call__(self, x):
-        lower = self._lower
-        upper = self._upper
-        if lower is not None:
-            for i, xs in enumerate(x):
-                if xs < lower[i] or xs > upper[i]:
-                    return -np.inf
         dict = self._dict_update(x)
-        return self._log_prob(self._u_to_c(dict), adjust_transform=True)
+        try:
+            return self._log_prob(self._u_to_c(dict), adjust_transform=True)
+        # if Pints proposes a value outside of Stan's parameter bounds
+        except RuntimeError:
+            return -np.inf
 
     def _dict_update(self, x):
         """ Updates dictionary object with parameter values. """
@@ -72,17 +62,13 @@ class StanLogPDF(InterfaceLogPDF):
 
     def evaluateS1(self, x):
         """ See :meth:`LogPDF.evaluateS1()`. """
-        lower = self._lower
-        upper = self._upper
-        if lower is not None:
-            for i, xs in enumerate(x):
-                j = self._index[i]
-                if xs < lower[j] or xs > upper[j]:
-                    return -np.inf, self._boundaries.sample()
         dict = self._dict_update(x)
-        uncons = self._u_to_c(dict)
-        return (self._log_prob(uncons, adjust_transform=True),
-                self._grad_log_prob(uncons, adjust_transform=True))
+        try:
+            uncons = self._u_to_c(dict)
+            return (self._log_prob(uncons, adjust_transform=True),
+                    self._grad_log_prob(uncons, adjust_transform=True))
+        except RuntimeError:
+            return -np.inf, np.ones(self._n_parameters)
 
     def _initialise_dict_index(self, names):
         """ Initialises dictionary and index of names. """
@@ -97,6 +83,13 @@ class StanLogPDF(InterfaceLogPDF):
         names_short = list(dict.fromkeys(names_short))
         index = [names_short.index(name) for name in names_long]
         return names_short, index
+
+    def pickled_form(self):
+        """ Returns form of model ready for pickling. """
+        outputs = {'model': self._compiled_stan,
+                   'fit': self._fit,
+                   'StanLogPDF': self}
+        return outputs
 
     def names(self):
         """ Returns names of Stan parameters. """
