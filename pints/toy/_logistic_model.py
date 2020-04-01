dc3d35d4dc3c0a605fc468d1cd718b11136c532d
Chon Lok Lei <chonloklei@gmail.com>
2020-04-02 06:07:21 +0800

    Update docs

--- pints/toy/_logistic_model.py
+++ pints/toy/_logistic_model.py
@@ -28,14 +28,15 @@ class LogisticModel(pints.ForwardModelS1, ToyModel):
                                          + \frac{1}{(k/p_0 - 1)\exp(-r t) + 1}
 
     Has two model parameters: A growth rate :math:`r` and a carrying capacity
-    :math:`k`.
+    :math:`k`. :math:`p_0` is the initial population size which is a fixed
+    (known) parameter in the model.
 
     Extends :class:`pints.ForwardModel`, :class:`pints.toy.ToyModel`.
 
     Parameters
     ----------
     initial_population_size : int
-        Sets the initial population size :math:`f(0)`.
+        Sets the initial population size :math:`f(0) = p_0`.
 
     References
     ----------
