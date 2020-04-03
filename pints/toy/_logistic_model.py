d24fc7f693970f6c4f4a35d74623ac34314626ba
Chon Lok Lei <chonloklei@gmail.com>
2020-04-03 08:50:18 +0800

    Tiny change to explain all symbols in the beginning

--- pints/toy/_logistic_model.py
+++ pints/toy/_logistic_model.py
@@ -28,15 +28,15 @@ class LogisticModel(pints.ForwardModelS1, ToyModel):
                                          + \frac{1}{(k/p_0 - 1)\exp(-r t) + 1}
 
     Has two model parameters: A growth rate :math:`r` and a carrying capacity
-    :math:`k`. The initial population size :math:`p_0` is a fixed (known)
-    parameter in the model.
+    :math:`k`. The initial population size :math:`p_0 = f(0)` is a fixed
+    (known) parameter in the model.
 
     Extends :class:`pints.ForwardModel`, :class:`pints.toy.ToyModel`.
 
     Parameters
     ----------
     initial_population_size : float
-        Sets the initial population size :math:`f(0) = p_0`.
+        Sets the initial population size :math:`p_0`.
 
     References
     ----------
