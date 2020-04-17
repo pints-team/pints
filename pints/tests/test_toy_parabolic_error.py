72253411711c2286c962239bf2e26810b080da4b
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-04-17 13:07:50 +0100

    Added value-based tests for parabolic error sensitivities.

--- pints/tests/test_toy_parabolic_error.py
+++ pints/tests/test_toy_parabolic_error.py
@@ -6,9 +6,13 @@
 # released under the BSD 3-clause license. See accompanying LICENSE.md for
 # copyright notice and full license details.
 #
+import unittest
+
+import numpy as np
+
 import pints
 import pints.toy
-import unittest
+
 
 
 class TestParabolicError(unittest.TestCase):
@@ -17,18 +21,34 @@ class TestParabolicError(unittest.TestCase):
     """
     def test_parabolic_error(self):
 
-        # Test basics
+        # Test a 2d case
         f = pints.toy.ParabolicError()
         self.assertEqual(f.n_parameters(), 2)
         self.assertEqual(list(f.optimum()), [0, 0])
         self.assertEqual(f([0, 0]), 0)
         self.assertTrue(f([0.1, 0.1]) > 0)
 
+        # Test a 3d case
         f = pints.toy.ParabolicError([1, 1, 1])
         self.assertEqual(f.n_parameters(), 3)
         self.assertEqual(list(f.optimum()), [1, 1, 1])
         self.assertEqual(f([1, 1, 1]), 0)
         self.assertTrue(f([1.1, 1.1, 1.1]) > 0)
+
+        # Test sensitivities
+        x = [1, 1, 1]
+        fx, dfx = f.evaluateS1(x)
+        self.assertEqual(fx, f(x))
+        self.assertEqual(dfx.shape, (3, ))
+        self.assertTrue(np.all(dfx == [0, 0, 0]))
+
+        x = [1.1, 1, 0.8]
+        fx, dfx = f.evaluateS1(x)
+        self.assertEqual(fx, f(x))
+        self.assertEqual(dfx.shape, (3, ))
+        self.assertAlmostEqual(dfx[0], 0.2)
+        self.assertAlmostEqual(dfx[1], 0)
+        self.assertAlmostEqual(dfx[2], -0.4)
 
 
 if __name__ == '__main__':
