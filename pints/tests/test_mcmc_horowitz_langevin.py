dc16ac5929af7ae1a085538c93f1a19eeb05d665
DavAug <david.augustin@gmx.net>
2020-05-15 13:02:23 +0200

    test functionalilty of langevinMCMC

---
+++ pints/tests/test_mcmc_horowitz_langevin.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+#
+# Tests the basic methods of the Horowitz Langevin MCMC routine.
+#
+# This file is part of PINTS (https://github.com/pints-team/pints/) which is
+# released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
+import unittest
+import numpy as np
+
+import pints
+import pints.toy
+
+from shared import StreamCapture
+
+
+class TestHorowitzLangevinMCMC(unittest.TestCase):
+    """
+    Tests the basic methods of the HorowitzLangevin MCMC routine.
+    """
+
+    def test_method(self):
+
+        # Create log pdf
+        log_pdf = pints.toy.GaussianLogPDF([5, 5], [[4, 1], [1, 3]])
+
+        # Create mcmc
+        x0 = np.array([2, 2])
+        sigma = [[3, 0], [0, 3]]
+        mcmc = pints.HorowitzLangevinMCMC(x0, sigma)
+
+        # This method needs sensitivities
+        self.assertTrue(mcmc.needs_sensitivities())
+
+        # Set number of steps
+        number_steps = 1000
+
+        # Perform short run
+        chain = []
+        for i in range(number_steps):
+            x = mcmc.ask()
+            fx, gr = log_pdf.evaluateS1(x)
+            sample = mcmc.tell((fx, gr))
+            if i >= 0.5 * number_steps and sample is not None:
+                chain.append(sample)
+            if np.all(sample == x):
+                self.assertEqual(mcmc.current_log_pdf(), fx)
+
+        chain = np.array(chain)
+        self.assertEqual(chain.shape[0], 0.5 * number_steps)
+        self.assertEqual(chain.shape[1], len(x0))
+
+    def test_logging(self):
+        # Test logging includes name and custom fields.
+
+        log_pdf = pints.toy.GaussianLogPDF([5, 5], [[4, 1], [1, 3]])
+        x0 = [np.array([2, 2]), np.array([8, 8])]
+
+        mcmc = pints.MCMCController(
+            log_pdf, 2, x0, method=pints.HorowitzLangevinMCMC)
+        mcmc.set_max_iterations(5)
+        with StreamCapture() as c:
+            mcmc.run()
+        text = c.text()
+
+        self.assertIn('Horowitz Langevin Algorithm', text)
+        self.assertIn(' Accept.', text)
+
+    def test_flow(self):
+
+        log_pdf = pints.toy.GaussianLogPDF([5, 5], [[4, 1], [1, 3]])
+        x0 = np.array([2, 2])
+
+        # Test initial proposal is first point
+        mcmc = pints.HorowitzLangevinMCMC(x0)
+        self.assertTrue(np.all(mcmc.ask() == mcmc._x0))
+
+        # Repeated asks
+        self.assertRaises(RuntimeError, mcmc.ask)
+
+        # Tell without ask
+        mcmc = pints.HorowitzLangevinMCMC(x0)
+        self.assertRaises(RuntimeError, mcmc.tell, 0)
+
+        # Repeated tells should fail
+        x = mcmc.ask()
+        mcmc.tell(log_pdf.evaluateS1(x))
+        self.assertRaises(RuntimeError, mcmc.tell, log_pdf.evaluateS1(x))
+
+        # Bad starting point
+        mcmc = pints.HorowitzLangevinMCMC(x0)
+        mcmc.ask()
+        self.assertRaises(
+            ValueError, mcmc.tell, (float('-inf'), np.array([1, 1])))
+
+    def test_set_hyper_parameters(self):
+        # Tests the parameter interface for this sampler.
+
+        x0 = np.array([2, 2])
+        mcmc = pints.HorowitzLangevinMCMC(x0)
+
+        # Test leapfrog parameters
+        d = mcmc.leapfrog_step_size()
+        self.assertTrue(len(d) == mcmc._n_parameters)
+
+        mcmc.set_leapfrog_step_size(0.5)
+        self.assertEqual(mcmc.leapfrog_step_size()[0], 0.5)
+        self.assertRaises(ValueError, mcmc.set_leapfrog_step_size, -1)
+
+        self.assertEqual(mcmc.n_hyper_parameters(), 1)
+        mcmc.set_hyper_parameters(2)
+        self.assertEqual(mcmc.leapfrog_step_size()[0], 2)
+
+        mcmc.set_epsilon(0.4)
+        self.assertEqual(mcmc.epsilon(), 0.4)
+        self.assertRaises(ValueError, mcmc.set_epsilon, -0.1)
+        mcmc.set_leapfrog_step_size(1)
+        self.assertEqual(len(mcmc.scaled_epsilon()), 2)
+        self.assertEqual(mcmc.scaled_epsilon()[0], 0.4)
+        self.assertEqual(len(mcmc.divergent_iterations()), 0)
+        self.assertRaises(ValueError, mcmc.set_leapfrog_step_size, [1, 2, 3])
+
+        mcmc.set_leapfrog_step_size([1.5, 3])
+        self.assertEqual(mcmc.leapfrog_step_size()[0], 1.5)
+        self.assertEqual(mcmc.leapfrog_step_size()[1], 3)
+
+    def test_other_setters(self):
+        # Tests other setters and getters.
+        x0 = np.array([2, 2])
+        mcmc = pints.HorowitzLangevinMCMC(x0)
+        self.assertRaises(ValueError, mcmc.set_hamiltonian_threshold, -0.3)
+        threshold1 = mcmc.hamiltonian_threshold()
+        self.assertEqual(threshold1, 10**3)
+        threshold2 = 10
+        mcmc.set_hamiltonian_threshold(threshold2)
+        self.assertEqual(mcmc.hamiltonian_threshold(), threshold2)
+
+
+if __name__ == '__main__':
+    unittest.main()
