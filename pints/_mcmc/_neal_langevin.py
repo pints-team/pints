b3a3f51b24820bd800ec2e30b1e11c70afca64b3
DavAug <david.augustin@gmx.net>
2020-05-17 17:25:55 +0200

    flake8

--- pints/_mcmc/_neal_langevin.py
+++ pints/_mcmc/_neal_langevin.py
@@ -290,7 +290,7 @@ class NealLangevinMCMC(pints.SingleChainMCMC):
         if sigma:
             if sigma <= 0:
                 raise ValueError(
-                    'The standard deviation of delta can only take non-
+                    'The standard deviation of delta can only take non-'
                     'negative values.')
             if sigma > 0:
                 self._sigma_delta = sigma
