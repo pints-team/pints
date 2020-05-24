a350cb6a6f46d61b0d88b3a34f69bca61017fa8a
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2020-05-24 18:49:15 +0100

    Removing none ASCII characters

--- pints/_optimisers/_bfgs_linesearch.py
+++ pints/_optimisers/_bfgs_linesearch.py
@@ -1,5 +1,5 @@
 #
-# Broyden–Fletcher–Goldfarb–Shanno algorithm
+# Broyden-Fletcher-Goldfarb-Shanno algorithm
 #
 # This file is part of PINTS (https://github.com/pints-team/pints/) which is
 # released under the BSD 3-clause license. See accompanying LICENSE.md for
