e0cdae267cdbe08f982b5f806315de682e1c4966
DavAug <david.augustin@gmx.net>
2020-08-11 15:26:59 +0200

    rename Combined to ConstantAndMultiplicative

--- pints/__init__.py
+++ pints/__init__.py
@@ -113,7 +113,7 @@ from ._log_likelihoods import (
     AR1LogLikelihood,
     ARMA11LogLikelihood,
     CauchyLogLikelihood,
-    CombinedGaussianLogLikelihood,
+    ConstantAndMultiplicativeGaussianLogLikelihood,
     GaussianIntegratedUniformLogLikelihood,
     GaussianKnownSigmaLogLikelihood,
     GaussianLogLikelihood,
