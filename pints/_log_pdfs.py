07f3fb301896bc7621fa7544413dce2cd4fc5020
DavAug <david.augustin@gmx.net>
2020-09-09 13:58:51 +0100

    flake 8

--- pints/_log_pdfs.py
+++ pints/_log_pdfs.py
@@ -172,7 +172,7 @@ class PooledLogPDF(LogPDF):
     Example
     -------
     ::
-    
+
         pooled_log_likelihood = pints.PooledLogPDF(
             log_pdfs=[
                 pints.GaussianLogLikelihood(problem1),
