e034d71c2812cdc947e5c8d34aab31413b586367
Chon Lok Lei <chonloklei@gmail.com>
2020-11-26 23:29:12 +0800

    Non-ASCII issue

--- pints/toy/__init__.py
+++ pints/toy/__init__.py
@@ -36,4 +36,4 @@ from ._simple_egg_box import SimpleEggBoxLogPDF
 from ._sir_model import SIRModel
 from ._twisted_gaussian_banana import TwistedGaussianLogPDF
 from ._stochastic_degradation_model import StochasticDegradationModel
-from ._stochastic_logistic_model import StochasticLogisticModel
+from ._stochastic_logistic model import StochasticLogisticModel
