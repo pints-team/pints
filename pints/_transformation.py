9a95c2476f48ec0c5ceda31b2324f72e30d128fd
Chon Lok Lei <chonloklei@gmail.com>
2021-02-08 23:52:14 +0800

    Keep half-open interval for RectangularBoundariesTransformation docs

--- pints/_transformation.py
+++ pints/_transformation.py
@@ -708,7 +708,7 @@ class LogTransformation(Transformation):
 class RectangularBoundariesTransformation(Transformation):
     r"""
     A generalised version of the logit transformation for the model parameters,
-    which transforms an interval or rectangular boundaries :math:`(a, b)` to
+    which transforms an interval or rectangular boundaries :math:`[a, b)` to
     all real number.
 
     The transformation is given by
@@ -718,9 +718,8 @@ class RectangularBoundariesTransformation(Transformation):
                  = \log(p - a) - \log(b - p),
 
     where :math:`p` is the model parameter vector and :math:`q` is the
-    search space vector. The range does not include the boundaries (:math:`a`
-    and :math:`b`). Note that :class:`LogitTransformation` is a special case
-    where :math:`a = 0` and :math:`b = 1`.
+    search space vector. Note that :class:`LogitTransformation` is a special
+    case where :math:`a = 0` and :math:`b = 1`.
 
     The Jacobian adjustment of the transformation is given by
 
