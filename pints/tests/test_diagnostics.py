ea122a4e8b2db5afea488444df6cccf446abb4c8
DavAug <david.augustin@gmx.net>
2021-02-16 20:28:10 +0100

    #1283 start testing of autocorrelation

--- pints/tests/test_diagnostics.py
+++ pints/tests/test_diagnostics.py
@@ -7,8 +7,10 @@
 # copyright notice and full license details.
 #
 import unittest
-import pints
 import numpy as np
+
+import pints
+import pints.diagnostics
 import pints._diagnostics
 
 # Unit testing in Python 2 and 3
@@ -19,6 +21,157 @@ except AttributeError:
 
 
 class TestDiagnostics(unittest.TestCase):
+    """
+    Tests various diagnostic measures available in Pints
+    """
+    def test_autocorrelation(self):
+        # Test case I: 100% correlated
+        n_chains = 3
+        n_iterations = 10
+        n_parameters = 2
+        chains = np.ones(shape=(n_chains, n_iterations, n_parameters))
+        autocorr = pints.diagnostics.autocorrelation(chains)
+
+        self.assertEqual(
+            autocorr.shape, (n_chains, n_iterations, n_parameters))
+        self.assertTrue(np.alltrue(autocorr == 1))
+
+        # Test case II: jump in samples
+        chains = np.array([1, 1, 1, -1, -1, -1])[np.newaxis, :, np.newaxis]
+        autocorr = pints.diagnostics.autocorrelation(chains)
+
+        n_samples = 6
+        var = 6 / (n_samples - 1)
+        self.assertEqual(autocorr.shape, (1, 6, 1))
+        autocorr = autocorr[0, :, 0]
+        self.assertAlmostEqual(autocorr[0], 6 / ((n_samples - 1) * var))
+        self.assertAlmostEqual(autocorr[1], 3 / ((n_samples - 1) * var))
+        self.assertAlmostEqual(autocorr[2], 0)
+        self.assertAlmostEqual(autocorr[3], -3 / ((n_samples - 1) * var))
+        self.assertAlmostEqual(autocorr[4], -2 / ((n_samples - 1) * var))
+        self.assertAlmostEqual(autocorr[5], -1 / ((n_samples - 1) * var))
+
+        # Test case III: non-trivial autocorrelation
+        chains = np.array([-2, 1, 0, -1, 2])[np.newaxis, :, np.newaxis]
+        autocorr = pints.diagnostics.autocorrelation(chains)
+
+        n_samples = 6
+        var = (4 + 1 + 1 + 4) / (n_samples - 1)
+        self.assertEqual(autocorr.shape, (1, 5, 1))
+        autocorr = autocorr[0, :, 0]
+        self.assertAlmostEqual(autocorr[0], 10 / ((n_samples - 1) * var))
+        self.assertAlmostEqual(autocorr[1], -4 / ((n_samples - 1) * var))
+        self.assertAlmostEqual(autocorr[2], -1 / ((n_samples - 1) * var))
+        self.assertAlmostEqual(autocorr[3], 4 / ((n_samples - 1) * var))
+        self.assertAlmostEqual(autocorr[4], -4 / ((n_samples - 1) * var))
+
+
+    # def test_effective_sample_size(self):
+    #     # Tests ess for a matrix of parameters
+
+    #     # matrix with two columns of samples
+    #     x = np.transpose(np.array([[1.0, 1.1, 1.4, 1.3, 1.3],
+    #                                [1.0, 2.0, 3.0, 4.0, 5.0]]))
+    #     y = pints._diagnostics.effective_sample_size(x)
+    #     self.assertAlmostEqual(y[0], 1.439232, 6)
+    #     self.assertAlmostEqual(y[1], 1.315789, 6)
+
+    #     # Bad calls
+    #     self.assertRaisesRegex(
+    #         ValueError, '2d array', pints.effective_sample_size, x[0])
+    #     self.assertRaisesRegex(
+    #         ValueError, 'At least two', pints.effective_sample_size, x[:1])
+
+    # def test_rhat(self):
+    #     # Tests that rhat works
+
+    #     # Tests Rhat computation for one parameter, chains.shape=(2, 4)
+    #     chains = np.array([[1.0, 1.1, 1.4, 1.3],
+    #                       [1.0, 2.0, 3.0, 4.0]])
+    #     self.assertAlmostEqual(
+    #         pints._diagnostics.rhat(chains), 2.3303847470550716, 6)
+
+    #     # Test Rhat computation for two parameters, chains.shape=(3, 4, 2)
+    #     chains = np.array([
+    #         [
+    #             [-1.10580535, 2.26589882],
+    #             [0.35604827, 1.03523364],
+    #             [-1.62581126, 0.47308597],
+    #             [1.03999619, 0.58203464]
+    #         ],
+    #         [
+    #             [-1.04755457, -2.28410098],
+    #             [0.17577692, -0.79433186],
+    #             [-0.07979098, -1.87816551],
+    #             [-1.39836319, 0.95119085]
+    #         ],
+    #         [
+    #             [-1.1182588, -0.34647435],
+    #             [1.36928142, -1.4079284],
+    #             [0.92272047, -1.49997615],
+    #             [0.89531238, 0.63207977]
+    #         ]])
+
+    #     y = pints._diagnostics.rhat(chains)
+    #     d = np.array(y) - np.array([0.84735944450487122, 1.1712652416950846])
+    #     self.assertLess(np.linalg.norm(d), 0.01)
+
+    # def test_bad_rhat_inputs(self):
+    #     # Tests whether exceptions are thrown should the input to rhat not be
+    #     # valid
+
+    #     # Pass chain of dimension 1
+    #     chains = np.empty(shape=1)
+    #     message = (
+    #         'Dimension of chains is 1. '
+    #         + 'Method computes Rhat for one '
+    #         'or multiple parameters and therefore only accepts 2 or 3 '
+    #         'dimensional arrays.')
+    #     self.assertRaisesRegex(
+    #         ValueError, message[0], pints.rhat, chains)
+
+    #     # Pass chain of dimension 4
+    #     chains = np.empty(shape=(1, 1, 1, 1))
+    #     message = (
+    #         'Dimension of chains is 4. '
+    #         + 'Method computes Rhat for one '
+    #         'or multiple parameters and therefore only accepts 2 or 3 '
+    #         'dimensional arrays.')
+    #     self.assertRaisesRegex(
+    #         ValueError, message[0], pints.rhat, chains)
+
+    #     # Pass bad warm-up arguments
+    #     chains = np.empty(shape=(2, 4))
+
+    #     # warm-up greater than 100%
+    #     warm_up = 1.1
+    #     message = (
+    #         '`warm_up` is set to 1.1. `warm_up` only takes values in [0,1].')
+    #     self.assertRaisesRegex(
+    #         ValueError, message[0], pints.rhat, chains, warm_up)
+
+    #     # Negative warm-up
+    #     warm_up = -0.1
+    #     message = (
+    #         '`warm_up` is set to -0.1. `warm_up` only takes values in [0,1].')
+    #     self.assertRaisesRegex(
+    #         ValueError, message[0], pints.rhat, chains, warm_up)
+
+    #     # Pass chains with too little samples (n<4)
+    #     chains = np.empty(shape=(1, 4))
+    #     warm_up = 0.9
+    #     message = (
+    #         'Number of samples per chain after warm-up and chain splitting is '
+    #         '1. Method needs at least 2 samples per chain.')
+    #     self.assertRaisesRegex(
+    #         ValueError, message[0], pints.rhat, chains, warm_up)
+
+    # def test_multivariate_rhat(self):
+    #     #TODO:
+    #     pass
+
+
+class TestDeprecatedDiagnostics(unittest.TestCase):
     """
     Tests various diagnostic measures available in Pints
     """
