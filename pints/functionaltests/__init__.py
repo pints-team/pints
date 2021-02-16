6e36010605a9ddf34d2484842abb198907bdfee7
DavAug <david.augustin@gmx.net>
2021-02-16 12:40:55 +0100

    #1294 add copyright notice

---
+++ pints/functionaltests/__init__.py
@@ -0,0 +1,5 @@
+#
+# This file is part of PINTS (https://github.com/pints-team/pints/) which is
+# released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
