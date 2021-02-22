3bc6fee37a2502fc8b6cdf95e3513ec812a4c313
Fergus Cooper <fcooper8472@gmail.com>
2021-02-22 22:53:03 +0000

    Make test importable

--- pints/functionaltests/__init__.py
+++ pints/functionaltests/__init__.py
@@ -3,3 +3,6 @@
 # released under the BSD 3-clause license. See accompanying LICENSE.md for
 # copyright notice and full license details.
 #
+
+from .functest_haario_bardenet_acmc import TestHaarioBardenetACMCOn2dimGaussianDistribution
+
