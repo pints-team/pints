f42b5cf35f4f3f9f8126c55dec2c3b444a8faa26
ben18785 <ben.c.lambert@gmail.com>
2021-03-22 23:59:45 +0000

    Added gaussian and banana tests for differential evolution

---
+++ pints/functionaltests/differential_evolution.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+#
+# This file is part of PINTS (https://github.com/pints-team/pints/) which is
+# released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
+
+from __future__ import division
+
+import pints
+
+from ._problems import (RunMcmcMethodOnTwoDimGaussian,
+                        RunMcmcMethodOnBanana)
+
+
+def test_differential_evolution_on_two_dim_gaussian():
+    problem = RunMcmcMethodOnTwoDimGaussian(
+        method=pints.DifferentialEvolutionMCMC,
+        n_chains=10,
+        n_iterations=10000,
+        n_warmup=1000
+    )
+
+    return {
+        'kld': problem.estimate_kld(),
+        'mean-ess': problem.estimate_mean_ess()
+    }
+
+
+def test_differential_evolution_on_banana():
+    problem = RunMcmcMethodOnBanana(
+        method=pints.DifferentialEvolutionMCMC,
+        n_chains=20,
+        n_iterations=5000,
+        n_warmup=1000
+    )
+
+    return {
+        'kld': problem.estimate_kld(),
+        'mean-ess': problem.estimate_mean_ess()
+    }
