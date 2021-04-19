e498d62149f9b2bfdb38cb662e7c3a116fd27028
ben18785 <ben.c.lambert@gmail.com>
2021-04-19 14:51:03 +0100

    Pinned Pystan version in setup and removed in actions
    Addresses #1335

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
             'traitlets',
         ],
         'stan': [
-            'pystan',
+            'pystan==2.19.1',
         ]
     },
     python_requires='>=2.7,!=3.0,!=3.1,!=3.2,!=3.3,!=3.4',
