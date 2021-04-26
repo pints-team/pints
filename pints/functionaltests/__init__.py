d1b0c9a8f85cf0c8bd3bee4ac117602ff84b6714
Fergus Cooper <fcooper8472@gmail.com>
2021-04-26 22:43:59 +0100

    expose some more functional tests

--- pints/functionaltests/__init__.py
+++ pints/functionaltests/__init__.py
@@ -4,4 +4,10 @@
 # copyright notice and full license details.
 #
 
+from .differential_evolution import test_differential_evolution_on_banana
+from .differential_evolution import test_differential_evolution_on_two_dim_gaussian
+
+from .haario_bardenet_acmc import test_haario_bardenet_acmc_on_annulus
+from .haario_bardenet_acmc import test_haario_bardenet_acmc_on_banana
+from .haario_bardenet_acmc import test_haario_bardenet_acmc_on_correlated_gaussian
 from .haario_bardenet_acmc import test_haario_bardenet_acmc_on_two_dim_gaussian
