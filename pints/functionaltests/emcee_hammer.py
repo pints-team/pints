7a7d9abc5f3b9d23604ed4ed86869d723a7e125c
ben18785 <ben.c.lambert@gmail.com>
2021-05-04 14:03:59 +0100

    Added problems for emcee hammer

--- pints/functionaltests/emcee_hammer.py
+++ pints/functionaltests/emcee_hammer.py
@@ -10,7 +10,9 @@ from __future__ import division
 import pints
 
 from ._problems import (RunMcmcMethodOnTwoDimGaussian,
-                        RunMcmcMethodOnBanana)
+                        RunMcmcMethodOnBanana,
+                        RunMcmcMethodOnCorrelatedGaussian,
+                        RunMcmcMethodOnAnnulus)
 
 
 def test_emcee_hammer_on_two_dim_gaussian(n_iterations=None):
@@ -18,7 +20,7 @@ def test_emcee_hammer_on_two_dim_gaussian(n_iterations=None):
         n_iterations = 10000
     problem = RunMcmcMethodOnTwoDimGaussian(
         method=pints.EmceeHammerMCMC,
-        n_chains=4,
+        n_chains=10,
         n_iterations=n_iterations,
         n_warmup=1000
     )
@@ -34,12 +36,44 @@ def test_emcee_hammer_on_banana(n_iterations=None):
         n_iterations = 10000
     problem = RunMcmcMethodOnBanana(
         method=pints.EmceeHammerMCMC,
-        n_chains=4,
+        n_chains=10,
         n_iterations=n_iterations,
         n_warmup=2000
     )
 
     return {
         'kld': problem.estimate_kld(),
+        'mean-ess': problem.estimate_mean_ess()
+    }
+
+
+def test_emcee_hammer_on_correlated_gaussian(n_iterations=None):
+    if n_iterations is None:
+        n_iterations = 8000
+    problem = RunMcmcMethodOnCorrelatedGaussian(
+        method=pints.EmceeHammerMCMC,
+        n_chains=10,
+        n_iterations=n_iterations,
+        n_warmup=4000
+    )
+
+    return {
+        'kld': problem.estimate_kld(),
+        'mean-ess': problem.estimate_mean_ess()
+    }
+
+
+def test_emcee_hammer_on_annulus(n_iterations=None):
+    if n_iterations is None:
+        n_iterations = 4000
+    problem = RunMcmcMethodOnAnnulus(
+        method=pints.EmceeHammerMCMC,
+        n_chains=10,
+        n_iterations=n_iterations,
+        n_warmup=2000
+    )
+
+    return {
+        'distance': problem.estimate_distance(),
         'mean-ess': problem.estimate_mean_ess()
     }
