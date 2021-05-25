9e48d20d0dadd464a91240815837560d1bc4675f
Chon Lok Lei <chonloklei@gmail.com>
2021-05-25 17:16:49 +0800

    Delete empty ending line

--- pints/functionaltests/__init__.py
+++ pints/functionaltests/__init__.py
@@ -11,4 +11,3 @@ from .haario_bardenet_acmc import test_haario_bardenet_acmc_on_annulus
 from .haario_bardenet_acmc import test_haario_bardenet_acmc_on_banana
 from .haario_bardenet_acmc import test_haario_bardenet_acmc_on_correlated_gaussian
 from .haario_bardenet_acmc import test_haario_bardenet_acmc_on_two_dim_gaussian
-
