77ab8df1f37e257c7019abd13362f3d8008e5f25
phumtutum <victor.popa2000@gmail.com>
2022-04-20 13:22:39 +0100

    made return similar to the one in ABC SMC

--- pints/_abc/_abc_rejection.py
+++ pints/_abc/_abc_rejection.py
@@ -70,7 +70,7 @@ class RejectionABC(pints.ABCSampler):
         if np.sum(accepted) == 0:
             return None
         else:
-            return [x.tolist() for c, x in
+            return [self._xs[c].tolist() for c, x in
                     enumerate(accepted) if x.all()]
 
     def threshold(self):
